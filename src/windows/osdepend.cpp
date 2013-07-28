6154f7fb066d1c0c2f465d9b7643a8688dbbbd99
loveemu <loveemu@gmail.com>
2013-07-28 23:56:22 +0900

    Fix access violation on DirectMusic initialization failure.

--- src/windows/osdepend.cpp
+++ src/windows/osdepend.cpp
@@ -4,5 +4,8 @@
 
 void ShowAlertMessage(wchar_t *msg)
 { 
-	MessageBox(pMainFrame->m_hWnd, msg, _T("Alert"), MB_OK);
+	if (pMainFrame != NULL && pMainFrame->m_hWnd != NULL)
+		MessageBox(pMainFrame->m_hWnd, msg, _T("Alert"), MB_OK);
+	else
+		fwprintf(stderr, L"%s\n", msg);
 }
