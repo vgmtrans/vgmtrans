36d3818636cc78020949d2e7467c04e10894287c
loveemu <loveemu@gmail.com>
2014-02-02 23:36:10 +0900

    Add loop point (smpl chunk) to RIFF WAVE output.

--- src/VGMSamp.cpp
+++ src/VGMSamp.cpp
@@ -87,8 +87,10 @@ bool VGMSamp::SaveAsWav(const wchar_t* filepath)
 
 	USHORT blockAlign = bps / 8*channels;
 
+	bool hasLoop = (this->loop.loopStatus != -1 && this->loop.loopStatus != 0);
+
 	PushTypeOnVectBE<UINT>(waveBuf, 0x52494646);			//"RIFF"
-	PushTypeOnVect<UINT>(waveBuf, bufSize+0x24);			//size
+	PushTypeOnVect<UINT>(waveBuf, 0x24 + bufSize + (hasLoop ? 0x50 : 0));	//size
 
 	//WriteLIST(waveBuf, 0x43564157, bufSize+24);			//write "WAVE" list
 	PushTypeOnVectBE<UINT>(waveBuf, 0x57415645);			//"WAVE"
@@ -107,6 +109,45 @@ bool VGMSamp::SaveAsWav(const wchar_t* filepath)
 	if (bufSize % 2)
 		waveBuf.push_back(0);
 
+	if (hasLoop)
+	{
+		const int origFormatBytesPerSamp = bps/8;
+		double compressionRatio = GetCompressionRatio();
+
+		// If the sample loops, but the loop length is 0, then assume the length should
+		// extend to the end of the sample.
+		ULONG loopLength = loop.loopLength;
+		if (loop.loopStatus && loop.loopLength == 0)
+		{
+			loopLength = dataLength - loop.loopStart;
+		}
+
+		ULONG loopStart =  (loop.loopStartMeasure==LM_BYTES) ?			//In sample chunk, the value is in number of samples
+			(ULONG)((loop.loopStart * compressionRatio) / origFormatBytesPerSamp) ://(16/8) :		//if the val is a raw offset of the original format, multiply it by the compression ratio
+			loop.loopStart /** origFormatBytesPerSamp * compressionRatio / origFormatBytesPerSamp*/;//(16/8);	 //if the value is in samples, multiply by bytes per sample to get raw offset of original format, then multiply by compression ratio to get raw offset in standard wav, then divide bytes bytesPerSamp of standard wave (2)
+		ULONG loopLenInSamp = (loop.loopLengthMeasure==LM_BYTES) ?		//In sample chunk, the value is in number of samples
+			(ULONG)((loopLength * compressionRatio) / origFormatBytesPerSamp) ://(16/8)  :	//if it's in raw bytes, multiply by compressionRatio to convert to Raw length in new format, and divide by bytesPerSamp of a 16 bit standard wave (16bits/8 bits per byte)
+			loopLength /** origFormatBytesPerSamp * compressionRatio) / origFormatBytesPerSamp*/;//(16/8);
+		ULONG loopEnd = loopStart + loopLenInSamp;
+
+		PushTypeOnVectBE<UINT>(waveBuf, 0x736D706C);		//"smpl"
+		PushTypeOnVect<UINT>(waveBuf, 0x50);				//size
+		PushTypeOnVect<UINT>(waveBuf, 0);					//manufacturer
+		PushTypeOnVect<UINT>(waveBuf, 0);					//product
+		PushTypeOnVect<UINT>(waveBuf, 1000000000/rate);		//sample period
+		PushTypeOnVect<UINT>(waveBuf, 60);					//MIDI uniti note (C5)
+		PushTypeOnVect<UINT>(waveBuf, 0);					//MIDI pitch fraction
+		PushTypeOnVect<UINT>(waveBuf, 0);					//SMPTE format
+		PushTypeOnVect<UINT>(waveBuf, 0);					//SMPTE offset
+		PushTypeOnVect<UINT>(waveBuf, 1);					//sample loops
+		PushTypeOnVect<UINT>(waveBuf, 0);					//sampler data
+		PushTypeOnVect<UINT>(waveBuf, 0);					//cue point ID
+		PushTypeOnVect<UINT>(waveBuf, 0);					//type (loop forward)
+		PushTypeOnVect<UINT>(waveBuf, loopStart);			//start sample #
+		PushTypeOnVect<UINT>(waveBuf, loopEnd);				//end sample #
+		PushTypeOnVect<UINT>(waveBuf, 0);					//fraction
+		PushTypeOnVect<UINT>(waveBuf, 0);					//playcount
+	}
 	
 	//DLSWave dlswave(1, channels, rate, rate*blockAlign, blockAlign, bps, bufSize, uncompSampBuf);
 	//dlswave.Write(waveBuf);
