6981b9512d32da002081d1fc0fdd5733bb1d86d7
loveemu <loveemu@gmail.com>
2014-07-27 14:41:54 +0900

    Add SeqVoiceAllocator class. It is intended to help to implement tie etc.

---
+++ src/SeqVoiceAllocator.cpp
@@ -0,0 +1,137 @@
+
+#include "stdafx.h"
+#include "SeqVoiceAllocator.h"
+
+SeqVoiceAllocator::SeqVoiceAllocator() :
+	time(0),
+	midiFile(NULL)
+{
+}
+
+SeqVoiceAllocator::~SeqVoiceAllocator()
+{
+}
+
+MidiFile * SeqVoiceAllocator::GetMidiFile()
+{
+	return midiFile;
+}
+
+void SeqVoiceAllocator::SetMidiFile(MidiFile * midiFile)
+{
+	this->midiFile = midiFile;
+}
+
+int32_t SeqVoiceAllocator::GetTime()
+{
+	return time;
+}
+
+void SeqVoiceAllocator::Tick()
+{
+	std::list<SeqNote>::iterator note;
+	std::list<SeqNote>::iterator nextNote;
+
+	note = notes.begin();
+	while (note != notes.end())
+	{
+		nextNote = note;
+		++nextNote;
+
+		// remove expired note
+		if (note->duration != 0 && note->time + note->duration >= time)
+		{
+			NoteOff(note);
+		}
+
+		note = nextNote;
+	}
+
+	time++;
+}
+
+void SeqVoiceAllocator::NoteOn(int8_t track, int8_t channel, int32_t time, int8_t key, int8_t velocity)
+{
+	SeqNote note(track, channel, time, key, velocity, 0);
+
+	notes.push_back(note);
+	if (midiFile != NULL) {
+		note.AddNoteOn(midiFile);
+	}
+}
+
+void SeqVoiceAllocator::NoteWithDur(int8_t track, int8_t channel, int32_t time, int8_t key, int8_t velocity, int32_t duration)
+{
+	SeqNote note(track, channel, time, key, velocity, duration);
+
+	notes.push_back(note);
+	if (midiFile != NULL) {
+		note.AddNoteOn(midiFile);
+	}
+}
+
+void SeqVoiceAllocator::NoteOff(std::list<SeqNote>::iterator note)
+{
+	if (midiFile != NULL) {
+		note->AddNoteOff(midiFile);
+	}
+	notes.erase(note);
+}
+
+void SeqVoiceAllocator::NoteOff(int8_t track, int8_t key)
+{
+	std::list<SeqNote>::iterator note;
+	std::list<SeqNote>::iterator nextNote;
+
+	note = notes.begin();
+	while (note != notes.end())
+	{
+		nextNote = note;
+		++nextNote;
+
+		if (note->track == track && note->key == key)
+		{
+			NoteOff(note);
+			break;
+		}
+
+		note = nextNote;
+	}
+}
+
+void SeqVoiceAllocator::TrackNotesOff(int8_t track)
+{
+	std::list<SeqNote>::iterator note;
+	std::list<SeqNote>::iterator nextNote;
+
+	note = notes.begin();
+	while (note != notes.end())
+	{
+		nextNote = note;
+		++nextNote;
+
+		if (note->track == track)
+		{
+			NoteOff(note);
+		}
+
+		note = nextNote;
+	}
+}
+
+void SeqVoiceAllocator::AllNotesOff()
+{
+	std::list<SeqNote>::iterator note;
+	std::list<SeqNote>::iterator nextNote;
+
+	note = notes.begin();
+	while (note != notes.end())
+	{
+		nextNote = note;
+		++nextNote;
+
+		NoteOff(note);
+
+		note = nextNote;
+	}
+}
