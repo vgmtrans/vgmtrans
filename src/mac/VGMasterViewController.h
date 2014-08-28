76648c279bcb246475b226db1b97d23c351d3d5c
mikelow <lowin.mike@gmail.com>
2014-08-27 21:40:47 -0400

    fixes and beginning of MacVGMRoot. Non-compiling

---
+++ src/mac/VGMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  VGMasterViewController.h
+//  VGMTrans
+//
+//  Created by Michael Lowin on 8/27/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VGMasterViewController : NSViewController
+
+@end
