897729bd685b2058f0bc0b725ee5f38b5656dd7e
loveemu <loveemu@gmail.com>
2015-01-13 19:32:47 +0900

    HexView now selects a smallest item as far as possible

--- src/windows/HexView.cpp
+++ src/windows/HexView.cpp
@@ -87,8 +87,12 @@ void CHexView::OnLButtonDown(UINT nFlags, CPoint point)
 			offset += (point.x - GetAsciiLeftEdge()) / CHAR_WIDTH;
 		}
 		VGMItem* selectedItem = NULL;
-		if (bClickedAnItem)
-			selectedItem = curFile->GetItemFromOffset(offset);
+		if (bClickedAnItem) {
+			selectedItem = curFile->GetItemFromOffset(offset, false);
+			if (selectedItem == NULL) {
+				selectedItem = curFile->GetItemFromOffset(offset, true);
+			}
+		}
 		parFrame->SelectItem(selectedItem);
 		curOffset = offset;
 	}
@@ -152,7 +156,7 @@ void CHexView::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
 		else if (curOffset >= endOffset)
 			curOffset = endOffset-1;
 
-		VGMItem* temp = curFile->GetItemFromOffset(curOffset);
+		VGMItem* temp = curFile->GetItemFromOffset(curOffset, false);
 		if (temp)
 		{
 			//winroot.SelectItem(temp);
