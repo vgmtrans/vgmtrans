34e03c3b1a9862f1053e9cfa12261f1d7feccfa1
loveemu <loveemu@gmail.com>
2015-02-18 01:17:43 +0900

    Add SNES Software Creation's format stub

---
+++ src/main/formats/SoftCreatSnesSeq.cpp
@@ -0,0 +1,192 @@
+#include "stdafx.h"
+#include "SoftCreatSnesSeq.h"
+#include "SoftCreatSnesFormat.h"
+#include "ScaleConversion.h"
+
+DECLARE_FORMAT(SoftCreatSnes);
+
+//  ****************
+//  SoftCreatSnesSeq
+//  ****************
+#define MAX_TRACKS  8
+#define SEQ_PPQN    48
+
+SoftCreatSnesSeq::SoftCreatSnesSeq(RawFile* file, SoftCreatSnesVersion ver, uint32_t seqdataOffset, uint8_t headerAlignSize, std::wstring newName)
+	: VGMSeq(SoftCreatSnesFormat::name, file, seqdataOffset, 0, newName), version(ver),
+	headerAlignSize(headerAlignSize)
+{
+	bLoadTickByTick = true;
+	bAllowDiscontinuousTrackData = true;
+	bUseLinearAmplitudeScale = true;
+
+	UseReverb();
+	AlwaysWriteInitialReverb(0);
+
+	LoadEventMap();
+}
+
+SoftCreatSnesSeq::~SoftCreatSnesSeq(void)
+{
+}
+
+void SoftCreatSnesSeq::ResetVars(void)
+{
+	VGMSeq::ResetVars();
+}
+
+bool SoftCreatSnesSeq::GetHeaderInfo(void)
+{
+	SetPPQN(SEQ_PPQN);
+
+	VGMHeader* header = AddHeader(dwOffset, headerAlignSize * MAX_TRACKS);
+	if (dwOffset + headerAlignSize * MAX_TRACKS > 0x10000) {
+		return false;
+	}
+
+	for (uint8_t trackIndex = 0; trackIndex < MAX_TRACKS; trackIndex++) {
+		uint32_t addrTrackLowPtr = dwOffset + (trackIndex * 2 * headerAlignSize);
+		uint32_t addrTrackHighPtr = addrTrackLowPtr + headerAlignSize;
+		if (addrTrackLowPtr + 1 > 0x10000 || addrTrackHighPtr + 1 > 0x10000) {
+			return false;
+		}
+
+		std::wstringstream trackName;
+		trackName << L"Track Pointer " << (trackIndex + 1);
+		header->AddSimpleItem(addrTrackLowPtr, 1, trackName.str() + L" (LSB)");
+		header->AddSimpleItem(addrTrackHighPtr, 1, trackName.str() + L" (MSB)");
+
+		uint16_t addrTrackStart = GetByte(addrTrackLowPtr) | (GetByte(addrTrackHighPtr) << 8);
+		if (addrTrackStart != 0xffff) {
+			SoftCreatSnesTrack* track = new SoftCreatSnesTrack(this, addrTrackStart);
+			aTracks.push_back(track);
+		}
+	}
+
+	return true;
+}
+
+bool SoftCreatSnesSeq::GetTrackPointers(void)
+{
+	return true;
+}
+
+void SoftCreatSnesSeq::LoadEventMap()
+{
+	if (version == SOFTCREATSNES_NONE) {
+		return;
+	}
+
+	// TODO: SoftCreatSnesSeq::LoadEventMap
+}
+
+
+//  ******************
+//  SoftCreatSnesTrack
+//  ******************
+
+SoftCreatSnesTrack::SoftCreatSnesTrack(SoftCreatSnesSeq* parentFile, long offset, long length)
+: SeqTrack(parentFile, offset, length)
+{
+	ResetVars();
+	bDetermineTrackLengthEventByEvent = true;
+	bWriteGenericEventAsTextEvent = false;
+}
+
+void SoftCreatSnesTrack::ResetVars(void)
+{
+	SeqTrack::ResetVars();
+}
+
+bool SoftCreatSnesTrack::ReadEvent(void)
+{
+	SoftCreatSnesSeq* parentSeq = (SoftCreatSnesSeq*)this->parentSeq;
+
+	uint32_t beginOffset = curOffset;
+	if (curOffset >= 0x10000) {
+		return false;
+	}
+
+	uint8_t statusByte = GetByte(curOffset++);
+	bool bContinue = true;
+
+	std::wstringstream desc;
+
+	SoftCreatSnesSeqEventType eventType = (SoftCreatSnesSeqEventType)0;
+	std::map<uint8_t, SoftCreatSnesSeqEventType>::iterator pEventType = parentSeq->EventMap.find(statusByte);
+	if (pEventType != parentSeq->EventMap.end()) {
+		eventType = pEventType->second;
+	}
+
+	switch (eventType)
+	{
+	case EVENT_UNKNOWN0:
+		desc << L"Event: 0x" << std::hex << std::setfill(L'0') << std::setw(2) << std::uppercase << (int)statusByte;
+		AddUnknown(beginOffset, curOffset-beginOffset, L"Unknown Event", desc.str());
+		break;
+
+	case EVENT_UNKNOWN1:
+	{
+		uint8_t arg1 = GetByte(curOffset++);
+		desc << L"Event: 0x" << std::hex << std::setfill(L'0') << std::setw(2) << std::uppercase << (int)statusByte
+			<< std::dec << std::setfill(L' ') << std::setw(0)
+			<< L"  Arg1: " << (int)arg1;
+		AddUnknown(beginOffset, curOffset-beginOffset, L"Unknown Event", desc.str());
+		break;
+	}
+
+	case EVENT_UNKNOWN2:
+	{
+		uint8_t arg1 = GetByte(curOffset++);
+		uint8_t arg2 = GetByte(curOffset++);
+		desc << L"Event: 0x" << std::hex << std::setfill(L'0') << std::setw(2) << std::uppercase << (int)statusByte
+			<< std::dec << std::setfill(L' ') << std::setw(0)
+			<< L"  Arg1: " << (int)arg1
+			<< L"  Arg2: " << (int)arg2;
+		AddUnknown(beginOffset, curOffset-beginOffset, L"Unknown Event", desc.str());
+		break;
+	}
+
+	case EVENT_UNKNOWN3:
+	{
+		uint8_t arg1 = GetByte(curOffset++);
+		uint8_t arg2 = GetByte(curOffset++);
+		uint8_t arg3 = GetByte(curOffset++);
+		desc << L"Event: 0x" << std::hex << std::setfill(L'0') << std::setw(2) << std::uppercase << (int)statusByte
+			<< std::dec << std::setfill(L' ') << std::setw(0)
+			<< L"  Arg1: " << (int)arg1
+			<< L"  Arg2: " << (int)arg2
+			<< L"  Arg3: " << (int)arg3;
+		AddUnknown(beginOffset, curOffset-beginOffset, L"Unknown Event", desc.str());
+		break;
+	}
+
+	case EVENT_UNKNOWN4:
+	{
+		uint8_t arg1 = GetByte(curOffset++);
+		uint8_t arg2 = GetByte(curOffset++);
+		uint8_t arg3 = GetByte(curOffset++);
+		uint8_t arg4 = GetByte(curOffset++);
+		desc << L"Event: 0x" << std::hex << std::setfill(L'0') << std::setw(2) << std::uppercase << (int)statusByte
+			<< std::dec << std::setfill(L' ') << std::setw(0)
+			<< L"  Arg1: " << (int)arg1
+			<< L"  Arg2: " << (int)arg2
+			<< L"  Arg3: " << (int)arg3
+			<< L"  Arg4: " << (int)arg4;
+		AddUnknown(beginOffset, curOffset-beginOffset, L"Unknown Event", desc.str());
+		break;
+	}
+
+	default:
+		desc << L"Event: 0x" << std::hex << std::setfill(L'0') << std::setw(2) << std::uppercase << (int)statusByte;
+		AddUnknown(beginOffset, curOffset-beginOffset, L"Unknown Event", desc.str());
+		pRoot->AddLogItem(new LogItem(std::wstring(L"Unknown Event - ") + desc.str(), LOG_LEVEL_ERR, std::wstring(L"SoftCreatSnesSeq")));
+		bContinue = false;
+		break;
+	}
+
+	//std::wostringstream ssTrace;
+	//ssTrace << L"" << std::hex << std::setfill(L'0') << std::setw(8) << std::uppercase << beginOffset << L": " << std::setw(2) << (int)statusByte  << L" -> " << std::setw(8) << curOffset << std::endl;
+	//OutputDebugString(ssTrace.str().c_str());
+
+	return bContinue;
+}
