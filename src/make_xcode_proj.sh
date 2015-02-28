36786759388cd6d79fd77df6f49635cb9a19832e
mikel <mikel@cameo.tv>
2015-02-28 01:41:52 -0500

    deleting a bunch of unneeded files

--- src/make_xcode_proj.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# a little script to fix project files for use with AppCode after qmake conversion
-
-qmake -spec macx-xcode VGMTrans.pro
-sed -i .bak 's/sourceTree = "<absolute>"/sourceTree = SOURCE_ROOT/' "VGMTrans.xcodeproj/project.pbxproj"
-rm VGMTrans.xcodeproj/project.pbxproj.bak
-
