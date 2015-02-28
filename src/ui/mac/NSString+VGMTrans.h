ecbae8de4b213aa187e811cc8a4ae1ee2fa0f14a
mikel <mikel@cameo.tv>
2015-02-28 01:43:45 -0500

    deleting more unneeded files

--- src/ui/mac/NSString+VGMTrans.h
+++
@@ -1,8 +0,0 @@
-@interface NSString (VGMTrans)
-
-+(NSString *)stringFromChar:(const char *)charText;
-+(const char *)charFromString:(NSString *)string;
-+(NSString *)stringFromWchar:(const wchar_t *)charText;
-+(const wchar_t *)wcharFromString:(NSString *)string;
-
-@end
