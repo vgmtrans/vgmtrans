e52b31abf03dc14078d59f322ed400915d52155d
mikel <mikel@cameo.tv>
2015-03-08 21:43:45 -0400

    - more formatting change reverts

--- src/main/formats/KonamiSnesScanner.h
+++ src/main/formats/KonamiSnesScanner.h
@@ -5,36 +5,36 @@
 enum KonamiSnesVersion : uint8_t; // see KonamiSnesFormat.h
 
 class KonamiSnesScanner :
-        public VGMScanner
+		public VGMScanner
 {
 public:
-    KonamiSnesScanner(void)
-    {
-        USE_EXTENSION(L"spc");
-    }
-    virtual ~KonamiSnesScanner(void)
-    {
-    }
+	KonamiSnesScanner(void)
+	{
+		USE_EXTENSION(L"spc");
+	}
+	virtual ~KonamiSnesScanner(void)
+	{
+	}
 
-    virtual void Scan(RawFile* file, void* info = 0);
-    void SearchForKonamiSnesFromARAM (RawFile* file);
-    void SearchForKonamiSnesFromROM (RawFile* file);
+	virtual void Scan(RawFile* file, void* info = 0);
+	void SearchForKonamiSnesFromARAM (RawFile* file);
+	void SearchForKonamiSnesFromROM (RawFile* file);
 
 private:
-    static BytePattern ptnSetSongHeaderAddressGG4;
-    static BytePattern ptnReadSongListPNTB;
-    static BytePattern ptnReadSongListAXE;
-    static BytePattern ptnReadSongListCNTR3;
-    static BytePattern ptnJumpToVcmdGG4;
-    static BytePattern ptnJumpToVcmdCNTR3;
-    static BytePattern ptnBranchForVcmd6xMDR2;
-    static BytePattern ptnBranchForVcmd6xCNTR3;
-    static BytePattern ptnSetDIRGG4;
-    static BytePattern ptnSetDIRCNTR3;
-    static BytePattern ptnLoadInstrJOP;
-    static BytePattern ptnLoadInstrGP;
-    static BytePattern ptnLoadInstrGG4;
-    static BytePattern ptnLoadInstrPNTB;
-    static BytePattern ptnLoadInstrCNTR3;
-    static BytePattern ptnLoadPercInstrGG4;
+	static BytePattern ptnSetSongHeaderAddressGG4;
+	static BytePattern ptnReadSongListPNTB;
+	static BytePattern ptnReadSongListAXE;
+	static BytePattern ptnReadSongListCNTR3;
+	static BytePattern ptnJumpToVcmdGG4;
+	static BytePattern ptnJumpToVcmdCNTR3;
+	static BytePattern ptnBranchForVcmd6xMDR2;
+	static BytePattern ptnBranchForVcmd6xCNTR3;
+	static BytePattern ptnSetDIRGG4;
+	static BytePattern ptnSetDIRCNTR3;
+	static BytePattern ptnLoadInstrJOP;
+	static BytePattern ptnLoadInstrGP;
+	static BytePattern ptnLoadInstrGG4;
+	static BytePattern ptnLoadInstrPNTB;
+	static BytePattern ptnLoadInstrCNTR3;
+	static BytePattern ptnLoadPercInstrGG4;
 };
