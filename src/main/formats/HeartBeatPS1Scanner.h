a0cc3054801663bf74b86bb3b769025a7f7654dc
loveemu <loveemu@gmail.com>
2015-03-16 20:59:45 +0900

    HeartBeatPS1Format: Add instrument scanner and add file header to the SEQq sequence

---
+++ src/main/formats/HeartBeatPS1Scanner.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Scanner.h"
+#include "HeartBeatPS1Seq.h"
+#include "Vab.h"
+
+class HeartBeatPS1Scanner :
+	public VGMScanner
+{
+public:
+	HeartBeatPS1Scanner(void);
+	virtual ~HeartBeatPS1Scanner(void);
+
+	virtual void Scan(RawFile* file, void* info = 0);
+	std::vector<VGMFile*> SearchForHeartBeatPS1VGMFile (RawFile* file);
+};
