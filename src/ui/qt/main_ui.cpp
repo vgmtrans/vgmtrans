11f3575cb34f2c90e19cb9170d8b17a4c1ee9488
sykhro <sykhro@protonmail.com>
2018-05-15 22:59:51 +0200

    [cmake] general cmake refactor, will use our own subtrees for libraries it can't find

--- src/ui/qt/main_ui.cpp
+++ src/ui/qt/main_ui.cpp
@@ -5,21 +5,11 @@
 #include "mainwindow.h"
 #include "QtVGMRoot.h"
 
-#ifdef Q_OS_WIN
-Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
-#endif
-
-#ifdef Q_OS_MAC
-Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin)
-#endif
-
-
 //! [main() function]
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     qApp->setStyleSheet("QSplitter::handle{background-color: #B8B8B8;}");
-//    QFile file(":/qss/default.qss");
     QFile file(":/qdarkstyle/style.qss");
     file.open(QFile::ReadOnly);
     QString styleSheet = QLatin1String(file.readAll());
