252ed27736569952f4e2d7560a9f7f027908ee1d
sykhro <eli@sykh.ro>
2018-11-13 17:07:03 +0100

    [qt] add a simple logo and window icon

--- src/ui/qt/MenuBar.cpp
+++ src/ui/qt/MenuBar.cpp
@@ -39,7 +39,8 @@ void MenuBar::AppendInfoMenu() {
   QMenu* info_dropdown = addMenu("Help");
   menu_about_dlg = info_dropdown->addAction("About VGMTrans");
   connect(menu_about_dlg, &QAction::triggered, [=] {
-    QMessageBox::about(QApplication::activeWindow(), "About VGMTrans",
-                       "Copyright (c) 2018 - The VGMTrans Team");
+    QMessageBox::about(QApplication::activeWindow(),
+                       "About VGMTrans",
+                       "Copyright (c) 2002-2018 - The VGMTrans Team, released under the Zlib license.");
   });
 }
--- src/ui/qt/main_ui.cpp
+++ src/ui/qt/main_ui.cpp
@@ -8,23 +8,19 @@
 #include "MainWindow.h"
 #include "QtVGMRoot.h"
 
-#ifdef Q_OS_WIN
-#include <QtPlugin>
-Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
-#endif
-
 int main(int argc, char *argv[]) {
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 
   QCoreApplication::setOrganizationName(QStringLiteral("VGMTrans"));
-  QCoreApplication::setApplicationName(QStringLiteral("vgmtrans"));
+  QCoreApplication::setApplicationName(QStringLiteral("VGMTrans"));
 
   QApplication app(argc, argv);
 
   qtVGMRoot.Init();
   MainWindow window;
+  window.setWindowIcon(QIcon(":/images/logo.png"));
   window.show();
 
   return app.exec();
