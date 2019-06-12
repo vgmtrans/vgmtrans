f45529814a328cdaa4fbaadf4c8042fab325c914
sykhro <eli@sykh.ro>
2019-06-12 20:37:48 +0200

    [build] fix cmake module for fluidsynth

--- cmake/FindFLUIDSYNTH.cmake
+++ cmake/FindFLUIDSYNTH.cmake
@@ -1,8 +1,35 @@
-# VGMTrans (c) 2018
-# Licensed under the zlib license
-# Check the included LICENSE.txt for details
+# FindFluidSynth
+# ---------
+#
+# Try to find the FluidSynth library.
+#
+# This will define the following variables:
+#
+# ``FLUIDSYNTH_FOUND``
+#     System has FluidSynth.
+#
+# ``FLUIDSYNTH_VERSION``
+#     The version of FluidSynth.
+#
+# ``FLUIDSYNTH_INCLUDE_DIRS``
+#     This should be passed to target_include_directories() if
+#     the target is not used for linking.
+#
+# ``FLUIDSYNTH_LIBRARIES``
+#     The FluidSynth library.
+#     This can be passed to target_link_libraries() instead of
+#     the ``FluidSynth::FluidSynth`` target
+#
+# If ``FLUIDSYNTH_FOUND`` is TRUE, the following imported target
+# will be available:
+#
+# ``FluidSynth::FluidSynth``
+#     The FluidSynth library
+#
 #=============================================================================
 # Copyright 2018 Christophe Giboudeaux <christophe@krop.fr>
+#
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
@@ -26,35 +53,50 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #=============================================================================
-# FindFLUIDSYNTH.cmake
-# Locate FluidSynth headers and libraries
-#  FLUIDSYNTH_INCLUDE_DIR    - Path to the FluidSynth headers
-#  FLUIDSYNTH_LIBRARIES      - Path to the FluidSynth libraries
-#  FLUIDSYNTH_FOUND          - The system has FluidSynth
-#  FLUIDSYNTH_VERSION_STRING - FluidSynth version present on the system
 
-find_path(FLUIDSYNTH_INCLUDE_DIR
-        NAMES fluidsynth.h
-)
+if(NOT ${FluidSynth_FIND_VERSION})
+  set(FluidSynth_FIND_VERSION "1.1.6")
+endif()
+
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_FluidSynth QUIET fluidsynth>=${FluidSynth_FIND_VERSION})
 
 find_library(FLUIDSYNTH_LIBRARIES
-        NAMES fluidsynth
+  NAMES fluidsynth
+  HINTS ${PC_FluidSynth_LIBDIR}
 )
 
-if(EXISTS "${FLUIDSYNTH_INCLUDE_DIR}/fluidsynth/version.h")
-    file(STRINGS "${FLUIDSYNTH_INCLUDE_DIR}/fluidsynth/version.h" _FLUIDSYNTH_VERSION_H
-            REGEX "^#define[ ]+FLUIDSYNTH_VERSION[ ]+\"[0-9].[0-9].[0-9]\"")
-    string(REGEX REPLACE "^#define[ ]+FLUIDSYNTH_VERSION[ ]+\"([0-9].[0-9].[0-9])\".*" "\\1" FLUIDSYNTH_VERSION_STRING "${_FLUIDSYNTH_VERSION_H}")
+find_path(FLUIDSYNTH_INCLUDE_DIRS
+  NAMES fluidsynth.h
+  HINTS ${PC_FluidSynth_INCLUDEDIR}
+)
+
+if(NOT FLUIDSYNTH_VERSION)
+  if(EXISTS "${FLUIDSYNTH_INCLUDE_DIRS}/fluidsynth/version.h")
+    file(STRINGS "${FLUIDSYNTH_INCLUDE_DIRS}/fluidsynth/version.h" _FLUIDSYNTH_VERSION_H
+         REGEX "^#define[ ]+FLUIDSYNTH_VERSION[ ]+\"[0-9].[0-9].[0-9]\"")
+    string(REGEX REPLACE "^#define[ ]+FLUIDSYNTH_VERSION[ ]+\"([0-9].[0-9].[0-9])\".*" "\\1" FLUIDSYNTH_VERSION "${_FLUIDSYNTH_VERSION_H}")
     unset(_FLUIDSYNTH_VERSION_H)
+  endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FluidSynth
-        FOUND_VAR FLUIDSYNTH_FOUND
-        REQUIRED_VARS FLUIDSYNTH_LIBRARIES FLUIDSYNTH_INCLUDE_DIR
-        VERSION_VAR FLUIDSYNTH_VERSION_STRING
+find_package_handle_standard_args(fluidsynth
+  FOUND_VAR FLUIDSYNTH_FOUND
+  REQUIRED_VARS FLUIDSYNTH_LIBRARIES FLUIDSYNTH_INCLUDE_DIRS
+  VERSION_VAR FLUIDSYNTH_VERSION
 )
 
-mark_as_advanced(
-        FLUIDSYNTH_LIBRARIES FLUIDSYNTH_INCLUDE_DIR
+if(FLUIDSYNTH_FOUND AND NOT TARGET fluidsynth)
+  add_library(fluidsynth UNKNOWN IMPORTED)
+  set_target_properties(fluidsynth PROPERTIES
+    IMPORTED_LOCATION "${FLUIDSYNTH_LIBRARIES}"
+    INTERFACE_INCLUDE_DIRECTORIES "${FLUIDSYNTH_INCLUDE_DIRS}"
+  )
+endif()
+
+include(FeatureSummary)
+set_package_properties(fluidsynth PROPERTIES
+  URL "http://www.fluidsynth.org/"
+  DESCRIPTION "A SoundFont Synthesizer"
 )
