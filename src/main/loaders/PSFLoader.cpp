3d6a3dab0204ddda329c4afac82513da0282b2ac
sykhro <eli@sykh.ro>
2020-01-31 13:28:55 +0100

    [core] handle PSFFile exceptions in PSFLoader

--- src/main/loaders/PSFLoader.cpp
+++ src/main/loaders/PSFLoader.cpp
@@ -10,6 +10,7 @@
 #include <fmt/compile.h>
 #include <fmt/format.h>
 
+#include "LogManager.h"
 #include "components/PSFFile2.h"
 
 constexpr int PSF1_VERSION = 0x1;
@@ -24,10 +25,8 @@ const std::unordered_map<int, size_t> data_offset = {{PSF1_VERSION, 0x800},
                                                      {NCSF_VERSION, 0x0}};
 
 void PSFLoader::apply(const RawFile *file) {
-    uint8_t sig[4];
-    file->GetBytes(0, 4, sig);
-    if (memcmp(sig, "PSF", 3) == 0) {
-        uint8_t version = sig[3];
+    if (std::equal(file->begin(), file->begin() + 3, "PSF")) {
+        uint8_t version = file->get<u8>(3);
         if (data_offset.find(version) != data_offset.end()) {
             psf_read_exe(file, version);
         }
@@ -39,34 +38,38 @@ void PSFLoader::apply(const RawFile *file) {
  * however the following is perfectly fine for our purposes.
  */
 void PSFLoader::psf_read_exe(const RawFile *file, int version) {
-    PSFFile2 psf(*file);
+    try {
+        PSFFile2 psf(*file);
+        std::filesystem::path basepath(file->path());
+        auto libtag = psf.tags().find("_lib");
+        if (libtag != psf.tags().end()) {
+            auto newpath = basepath.replace_filename(libtag->second).string();
+            auto newfile = std::make_shared<DiskFile>(newpath);
+            enqueue(newfile);
 
-    std::filesystem::path basepath(file->path());
-    auto libtag = psf.tags().find("_lib");
-    if (libtag != psf.tags().end()) {
-        auto newpath = basepath.replace_filename(libtag->second).string();
-        auto newfile = std::make_shared<DiskFile>(newpath);
-        enqueue(newfile);
-
-        /* Look for additional libraries in the same folder */
-        auto libstring = fmt::compile("_lib{:d}");
-        int i = 1;
-        while (true) {
-            libtag = psf.tags().find(fmt::format(libstring, i++));
-            if (libtag != psf.tags().end()) {
-                newpath = basepath.replace_filename(libtag->second).string();
-                newfile = std::make_shared<DiskFile>(newpath);
-                enqueue(newfile);
-            } else {
-                break;
+            /* Look for additional libraries in the same folder */
+            auto libstring = fmt::compile("_lib{:d}");
+            int i = 1;
+            while (true) {
+                libtag = psf.tags().find(fmt::format(libstring, i++));
+                if (libtag != psf.tags().end()) {
+                    newpath = basepath.replace_filename(libtag->second).string();
+                    newfile = std::make_shared<DiskFile>(newpath);
+                    enqueue(newfile);
+                } else {
+                    break;
+                }
             }
         }
-    }
 
-    if (!psf.exe().empty()) {
-        auto newfile = std::make_shared<VirtFile>(
-            reinterpret_cast<const u8 *>(psf.exe().data()) + data_offset.at(version),
-            psf.exe().size() - data_offset.at(version), file->name(), file->path(), file->tag);
-        enqueue(newfile);
+        if (!psf.exe().empty()) {
+            auto newfile = std::make_shared<VirtFile>(
+                reinterpret_cast<const u8 *>(psf.exe().data()) + data_offset.at(version),
+                psf.exe().size() - data_offset.at(version), file->name(), file->path(), file->tag);
+            enqueue(newfile);
+        }
+    } catch (std::exception e) {
+        L_ERROR(e.what());
+        return;
     }
 }
