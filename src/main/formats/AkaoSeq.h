49142f62758163b3e5f27642c847945e8fb2cf70
loveemu <loveemu@gmail.com>
2020-05-23 00:37:10 +0900

    AKAO (PS1): Add a ton of opcode definitions for Final Fantasy 7

--- src/main/formats/AkaoSeq.h
+++ src/main/formats/AkaoSeq.h
@@ -17,23 +17,134 @@ enum class AkaoPs1Version : uint8_t {
   VERSION_2
 };
 
-static const unsigned short
-    delta_time_table[] = {0xC0, 0x60, 0x30, 0x18, 0x0C, 0x6, 0x3, 0x20, 0x10, 0x8, 0x4, 0x0, 0xA0A0, 0xA0A0};
-
+enum AkaoSeqEventType {
+  //start enum at 1 because if map[] look up fails, it returns 0, and we don't want that to get confused with a legit event
+  EVENT_UNIMPLEMENTED = 1,
+  EVENT_END,
+  EVENT_PROGCHANGE,
+  EVENT_ONE_TIME_DURATION,
+  EVENT_VOLUME,
+  EVENT_PITCH_SLIDE,
+  EVENT_OCTAVE,
+  EVENT_INCREMENT_OCTAVE,
+  EVENT_DECREMENT_OCTAVE,
+  EVENT_EXPRESSION,
+  EVENT_EXPRESSION_FADE,
+  EVENT_PAN,
+  EVENT_PAN_FADE,
+  EVENT_NOISE_CLOCK,
+  EVENT_ADSR_ATTACK_RATE,
+  EVENT_ADSR_DECAY_RATE,
+  EVENT_ADSR_SUSTAIN_LEVEL,
+  EVENT_ADSR_DECAY_RATE_AND_SUSTAIN_LEVEL,
+  EVENT_ADSR_SUSTAIN_RATE,
+  EVENT_ADSR_RELEASE_RATE,
+  EVENT_RESET_ADSR,
+  EVENT_VIBRATO,
+  EVENT_VIBRATO_DEPTH,
+  EVENT_VIBRATO_OFF,
+  EVENT_ADSR_ATTACK_MODE,
+  EVENT_TREMOLO,
+  EVENT_TREMOLO_DEPTH,
+  EVENT_TREMOLO_OFF,
+  EVENT_ADSR_SUSTAIN_MODE,
+  EVENT_PAN_LFO,
+  EVENT_PAN_LFO_DEPTH,
+  EVENT_PAN_LFO_OFF,
+  EVENT_ADSR_RELEASE_MODE,
+  EVENT_TRANSPOSE_ABS,
+  EVENT_TRANSPOSE_REL,
+  EVENT_REVERB_ON,
+  EVENT_REVERB_OFF,
+  EVENT_NOISE_ON,
+  EVENT_NOISE_OFF,
+  EVENT_PITCH_MOD_ON,
+  EVENT_PITCH_MOD_OFF,
+  EVENT_LOOP_START,
+  EVENT_LOOP_UNTIL,
+  EVENT_LOOP_AGAIN,
+  EVENT_RESET_VOICE_EFFECTS,
+  EVENT_SLUR_ON,
+  EVENT_SLUR_OFF,
+  EVENT_NOISE_ON_DELAY_TOGGLE,
+  EVENT_NOISE_DELAY_TOGGLE,
+  EVENT_LEGATO_ON,
+  EVENT_LEGATO_OFF,
+  EVENT_PITCH_MOD_ON_DELAY_TOGGLE,
+  EVENT_PITCH_MOD_DELAY_TOGGLE,
+  EVENT_D4,
+  EVENT_D5,
+  EVENT_D6,
+  EVENT_D7,
+  EVENT_TUNING_ABS,
+  EVENT_TUNING_REL,
+  EVENT_PORTAMENTO_ON,
+  EVENT_PORTAMENTO_OFF,
+  EVENT_FIXED_DURATION,
+  EVENT_VIBRATO_DEPTH_FADE,
+  EVENT_TREMOLO_DEPTH_FADE,
+  EVENT_PAN_LFO_FADE,
+  EVENT_E0_V2,
+  EVENT_E1_V2,
+  EVENT_E2_V2,
+  EVENT_E4_V2,
+  EVENT_E5_V2,
+  EVENT_E6_V2,
+  EVENT_TEMPO,
+  EVENT_TEMPO_FADE,
+  EVENT_REVERB_DEPTH,
+  EVENT_REVERB_DEPTH_FADE,
+  EVENT_DRUM_ON_V1,
+  EVENT_DRUM_ON_V2,
+  EVENT_DRUM_OFF,
+  EVENT_UNCONDITIONAL_JUMP,
+  EVENT_CPU_CONDITIONAL_JUMP,
+  EVENT_LOOP_BRANCH,
+  EVENT_LOOP_BREAK,
+  EVENT_PROGCHANGE_NO_ATTACK,
+  EVENT_F3_V1,
+  EVENT_UNISON_ON,
+  EVENT_UNISON_OFF,
+  EVENT_UNISON_VOLUME_BALANCE,
+  EVENT_UNISON_VOLUME_BALANCE_FADE,
+  EVENT_FE_0A_V2,
+  EVENT_FE_0B_V2,
+  EVENT_FE_0E_V2,
+  EVENT_FE_0F_V2,
+  EVENT_FE_10_V2,
+  EVENT_FE_11_V2,
+  EVENT_FE_12_V2,
+  EVENT_FE_14_V2,
+  EVENT_ALTERNATE_VOICE_ON,
+  EVENT_ALTERNATE_VOICE_OFF,
+  EVENT_TIME_SIGNATURE,
+  EVENT_MEASURE,
+  EVENT_FE_19_V2,
+  EVENT_FE_1A_V2,
+  EVENT_FE_1B_V2,
+  EVENT_FE_1C_V2,
+  EVENT_FE_1D_V2,
+  EVENT_FE_1E_V2
+};
 
 class AkaoSeq:
     public VGMSeq {
  public:
   AkaoSeq(RawFile *file, uint32_t offset);
-  virtual ~AkaoSeq(void);
+  virtual ~AkaoSeq();
 
-  virtual bool GetHeaderInfo();
-  virtual bool GetTrackPointers(void);
+  virtual bool GetHeaderInfo() override;
+  virtual bool GetTrackPointers() override;
+
+  std::wstring ReadTimestampAsText();
+  double GetTempoInBPM(uint16_t tempo) const;
 
   static AkaoPs1Version GuessVersion(RawFile *file, uint32_t offset);
   static uint32_t ReadNumOfTracks(RawFile *file, uint32_t offset);
 
-private:
+ private:
+  void LoadEventMap();
+
   static uint8_t GetNumPositiveBits(uint32_t ulWord) {
     return   ((ulWord & 0x80000000)>0) + ((ulWord & 0x40000000)>0) + ((ulWord & 0x20000000)>0) + ((ulWord & 0x10000000)>0)
       + ((ulWord & 0x8000000)>0) + ((ulWord & 0x4000000)>0) + ((ulWord & 0x2000000)>0) + ((ulWord & 0x1000000)>0)
@@ -50,6 +161,9 @@ private:
   uint16_t seq_id;
   AkaoPs1Version version;
 
+  std::map<uint8_t, AkaoSeqEventType> event_map;
+  std::map<uint8_t, AkaoSeqEventType> sub_event_map;
+
   bool bUsesIndividualArts;
 };
 
@@ -58,20 +172,25 @@ class AkaoTrack
  public:
   AkaoTrack(AkaoSeq *parentFile, long offset = 0, long length = 0);
 
-  virtual void ResetVars(void);
-  virtual bool ReadEvent(void);
+  virtual void ResetVars() override;
+  virtual bool ReadEvent() override;
+
+  AkaoSeq * seq() const {
+    return reinterpret_cast<AkaoSeq*>(this->parentSeq);
+  }
 
  public:
 
 
  protected:
-  uint8_t relative_key, base_key;
-  uint32_t current_delta_time;
-  unsigned long loop_begin_loc[8];
-  unsigned long loopID[8];
-  int loop_counter[8];
-  int loop_layer;
-  int loop_begin_layer;
-  bool bNotePlaying;
-  std::vector<uint32_t> vCondJumpAddr;
+  bool slur;
+  bool legato;
+  uint32_t loop_begin_loc[4];
+  uint16_t loop_layer;
+  uint16_t loop_counter[4];
+  uint16_t last_delta_time;
+  bool use_one_time_delta_time;
+  uint8_t one_time_delta_time;
+  uint16_t delta_time_overwrite;
+  int8_t tuning;
 };
