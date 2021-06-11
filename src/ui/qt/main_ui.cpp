bdc3c6de3f84d1d9121d5d65e37ce5dcdb452a90
Eli M <eli@meluc.ci>
2021-06-11 12:58:13 +0200

    MainWindow: correct filename casing

--- src/ui/qt/main_ui.cpp
+++ src/ui/qt/main_ui.cpp
@@ -8,17 +8,11 @@
 #include <QtPlugin>
 #include <QFile>
 #include "dropsitewindow.h"
-#include "mainwindow.h"
+#include "MainWindow.h"
 #include "QtVGMRoot.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    qApp->setStyleSheet("QSplitter::handle{background-color: #B8B8B8;}");
-
-    QFile file(":/qdarkstyle/style.qss");
-    file.open(QFile::ReadOnly);
-    QString styleSheet = QLatin1String(file.readAll());
-    qApp->setStyleSheet(styleSheet);
 
     qtVGMRoot.Init();
 
--- src/ui/qt/main_ui.h
+++
@@ -1,6 +0,0 @@
-#ifndef VGMTRANS_UI_MAIN_H
-#define VGMTRANS_UI_MAIN_H
-
-//int main_ui(int argc, char *argv[]);
-
-#endif //VGMTRANS_UI_MAIN_H
