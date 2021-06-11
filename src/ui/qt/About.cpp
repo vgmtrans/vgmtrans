3749058f72b68bedd86e176bf82158ec91278cd0
Eli M <eli@meluc.ci>
2021-06-11 13:21:57 +0200

    VGMTransQt: implement about dialog

---
+++ src/ui/qt/About.cpp
@@ -0,0 +1,55 @@
+/**
+ * VGMTrans (c) - 2002-2021
+ * Licensed under the zlib license
+ * See the included LICENSE for more information
+ */
+
+#include "About.h"
+
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QPixmap>
+
+About::About(QWidget *parent) : QDialog(parent) {
+    setWindowTitle("About VGMTrans");
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+
+    const QString small =
+        QStringLiteral("<p style='margin-top:0; margin-bottom:0; font-size:small;'>");
+    const QString medium = QStringLiteral("<p style='margin-top:15px;'>");
+
+    QString text;
+    text.append(QStringLiteral("<p style='font-size:38pt; font-weight:300; margin-bottom:0;'>VGMTrans</p>"));
+
+    text.append(medium + "An open-source videogame music translator</p>");
+    text.append(
+        medium +
+        QStringLiteral("<a href='https://github.com/vgmtrans/vgmtrans'>Source code</a>"));
+
+    QLabel *text_label = new QLabel(text);
+    text_label->setTextInteractionFlags(Qt::TextBrowserInteraction);
+    text_label->setOpenExternalLinks(true);
+    text_label->setContentsMargins(30, 0, 30, 0);
+
+    QLabel *copyright =
+        new QLabel(small + "\u00A9 2002-2021 VGMTrans Team | Licensed under the zlib license" +
+                   QStringLiteral("</p>"));
+
+    QLabel *logo = new QLabel();
+    logo->setPixmap(QPixmap(":/appicon.png").scaledToHeight(300, Qt::SmoothTransformation));
+    logo->setContentsMargins(30, 0, 30, 0);
+
+    QVBoxLayout *main_layout = new QVBoxLayout;
+    QHBoxLayout *h_layout = new QHBoxLayout;
+
+    setLayout(main_layout);
+    main_layout->addLayout(h_layout);
+    main_layout->addWidget(copyright);
+    copyright->setAlignment(Qt::AlignCenter);
+    copyright->setContentsMargins(0, 15, 0, 0);
+
+    h_layout->setAlignment(Qt::AlignLeft);
+    h_layout->addWidget(logo);
+    h_layout->addWidget(text_label);
+}
---
+++ src/ui/qt/About.h
@@ -0,0 +1,15 @@
+/**
+ * VGMTrans (c) - 2002-2021
+ * Licensed under the zlib license
+ * See the included LICENSE for more information
+ */
+
+#pragma once
+
+#include <QDialog>
+
+class About final : public QDialog {
+    Q_OBJECT
+   public:
+    explicit About(QWidget *parent = nullptr);
+};
