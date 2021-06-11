62a2a574884a44ce942b76fb54d6a78977186140
Eli M <eli@meluc.ci>
2021-06-11 13:19:42 +0200

    VGMTransQt: MainWindow: refactor constructor, cleanup the drop event, set title and icon

--- src/ui/qt/MainWindow.cpp
+++ src/ui/qt/MainWindow.cpp
@@ -1,6 +1,13 @@
-#include <qtextedit.h>
+/**
+ * VGMTrans (c) - 2002-2021
+ * Licensed under the zlib license
+ * See the included LICENSE for more information
+ */
+
 #include <QDragEnterEvent>
 #include <QMimeData>
+#include <QFileInfo>
+#include <QSplitter>
 #include "MainWindow.h"
 #include "QtVGMRoot.h"
 #include "workarea/RawFileListView.h"
@@ -14,109 +21,66 @@ const int defaultCollListHeight = 140;
 const int defaultFileListWidth = 200;
 const int splitterHandleWidth = 1;
 
-MainWindow::MainWindow(QWidget *parent)
-        : QMainWindow(parent)
-{
-    setAcceptDrops(true);
+MainWindow::MainWindow() : QMainWindow(nullptr) {
+  setWindowTitle("VGMTrans");
+  setWindowIcon(QIcon(":/appicon.png"));
 
-    rawFileListView = new RawFileListView();
-    vgmFileListView = new VGMFileListView();
-    vgmCollListView = new VGMCollListView();
+  setUnifiedTitleAndToolBarOnMac(true);
+  setAcceptDrops(true);
+  setContextMenuPolicy(Qt::NoContextMenu);
 
-    vertSplitter = new QSplitter(Qt::Vertical, this);
-    horzSplitter = new QSplitter(Qt::Horizontal, vertSplitter);
-    vertSplitterLeft = new QSplitter(Qt::Vertical, horzSplitter);
+  rawFileListView = new RawFileListView();
+  vgmFileListView = new VGMFileListView();
+  vgmCollListView = new VGMCollListView();
 
-    QList<int> sizes({defaultWindowHeight - defaultCollListHeight, defaultCollListHeight});
-    vertSplitter->addWidget(horzSplitter);
-    vertSplitter->addWidget(vgmCollListView);
-    vertSplitter->setStretchFactor(0, 1);
-    vertSplitter->setSizes(sizes);
-    vertSplitter->setHandleWidth(splitterHandleWidth);
-//    vertSplitter->setOpaqueResize(false);
+  vertSplitter = new QSplitter(Qt::Vertical, this);
+  horzSplitter = new QSplitter(Qt::Horizontal, vertSplitter);
+  vertSplitterLeft = new QSplitter(Qt::Vertical, horzSplitter);
 
-    sizes = QList<int>({defaultFileListWidth, defaultWindowWidth - defaultFileListWidth});
-    horzSplitter->addWidget(vertSplitterLeft);
-    horzSplitter->addWidget(MdiArea::getInstance());
-    horzSplitter->setStretchFactor(1, 1);
-    horzSplitter->setSizes(sizes);
-    horzSplitter->setHandleWidth(splitterHandleWidth);
-    horzSplitter->setMinimumSize(100, 100);
-    horzSplitter->setMaximumSize(500, 0);
-    horzSplitter->setCollapsible(0, false);
-    horzSplitter->setCollapsible(1, false);
+  QList<int> sizes({defaultWindowHeight - defaultCollListHeight, defaultCollListHeight});
+  vertSplitter->addWidget(horzSplitter);
+  vertSplitter->addWidget(vgmCollListView);
+  vertSplitter->setStretchFactor(0, 1);
+  vertSplitter->setSizes(sizes);
+  vertSplitter->setHandleWidth(splitterHandleWidth);
 
-    vertSplitterLeft->addWidget(rawFileListView);
-    vertSplitterLeft->addWidget(vgmFileListView);
-    vertSplitterLeft->setHandleWidth(splitterHandleWidth);
+  sizes = QList<int>({defaultFileListWidth, defaultWindowWidth - defaultFileListWidth});
+  horzSplitter->addWidget(vertSplitterLeft);
+  horzSplitter->addWidget(MdiArea::getInstance());
+  horzSplitter->setStretchFactor(1, 1);
+  horzSplitter->setSizes(sizes);
+  horzSplitter->setHandleWidth(splitterHandleWidth);
+  horzSplitter->setMinimumSize(100, 100);
+  horzSplitter->setMaximumSize(500, 0);
+  horzSplitter->setCollapsible(0, false);
+  horzSplitter->setCollapsible(1, false);
 
-    setCentralWidget(vertSplitter);
-    resize(defaultWindowWidth, defaultWindowHeight);
+  vertSplitterLeft->addWidget(rawFileListView);
+  vertSplitterLeft->addWidget(vgmFileListView);
+  vertSplitterLeft->setHandleWidth(splitterHandleWidth);
+
+  setCentralWidget(vertSplitter);
+  resize(defaultWindowWidth, defaultWindowHeight);
 }
 
-MainWindow::~MainWindow()
-{
-
+void MainWindow::dragEnterEvent(QDragEnterEvent *event) {
+  event->acceptProposedAction();
 }
 
-void MainWindow::dragEnterEvent(QDragEnterEvent *event)
-{
-    event->acceptProposedAction();
+void MainWindow::dragMoveEvent(QDragMoveEvent *event) {
+  event->acceptProposedAction();
 }
 
-void MainWindow::dragMoveEvent(QDragMoveEvent* event)
-{
-    event->acceptProposedAction();
-}
+void MainWindow::dropEvent(QDropEvent *event) {
+    const auto &files = event->mimeData()->urls();
 
-wchar_t* qstringTowchar_t(QString text){
-//    qDebug()<<text.length();
-    wchar_t* c_Text = new wchar_t[text.length() + 1];
-    text.toWCharArray(c_Text);
+    if (files.isEmpty())
+        return;
 
-    c_Text[text.length()] = 0; //Add this line should work as you expected
-    return c_Text;
-}
-
-void MainWindow::dropEvent(QDropEvent *event)
-{
-
-    const QMimeData *mimeData = event->mimeData();
-
-//    if (mimeData->hasImage()) {
-//        setPixmap(qvariant_cast<QPixmap>(mimeData->imageData()));
-//    } else if (mimeData->hasHtml()) {
-//        setText(mimeData->html());
-//        setTextFormat(Qt::RichText);
-//    } else
-    if (mimeData->hasText()) {
-        std::string utf8_text = mimeData->text().toUtf8().constData();
-        printf(utf8_text.c_str());
-//        setText(mimeData->text());
-//        setTextFormat(Qt::PlainText);
+    for (const auto &file : files) {
+        qtVGMRoot.OpenRawFile(QFileInfo(file.toLocalFile()).filePath().toStdWString());
     }
-    if (mimeData->hasUrls()) {
-        QList<QUrl> urlList = mimeData->urls();
-        int urlSize = urlList.size();
-        printf("%d", urlSize);
-        QString text;
-        for (int i = 0; i < urlList.size() && i < 32; ++i) {
-            QString url = urlList.at(i).toLocalFile();
-            wchar_t *str = qstringTowchar_t(url);
-//            qDebug() << text.length();
-            qtVGMRoot.OpenRawFile(str);
-//            text += url + QString("\n");
-        }
-        printf(text.toUtf8().constData());
-//        qtVGMRoot.OpenRawFile(urlList)
-//        setText(text);
-    } else {
-//        setText(tr("Cannot display data"));
-    }
-//! [dropEvent() function part2]
 
-//! [dropEvent() function part3]
     setBackgroundRole(QPalette::Dark);
     event->acceptProposedAction();
-
 }
--- src/ui/qt/MainWindow.h
+++ src/ui/qt/MainWindow.h
@@ -1,41 +1,39 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+/**
+ * VGMTrans (c) - 2002-2021
+ * Licensed under the zlib license
+ * See the included LICENSE for more information
+ */
+
+#pragma once
 
 #include <QMainWindow>
-#include <qsplitter.h>
-#include <qtreeview.h>
-#include <qlistview.h>
-#include <QMdiArea>
 #include "workarea/VGMFileView.h"
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class QSplitter;
+class QListView;
+
+class MainWindow final : public QMainWindow {
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+  MainWindow();
 
-    static MainWindow& getInstance() {
-        static MainWindow instance;
-        return instance;
-    }
-
-protected:
-    QSplitter *vertSplitter;
-    QSplitter *horzSplitter;
-    QSplitter *vertSplitterLeft;
-    QListView *rawFileListView;
-    QListView *vgmFileListView;
-    QListView *vgmCollListView;
-    QListView *collListView;
+  static MainWindow &getInstance() {
+    static MainWindow instance;
+    return instance;
+  }
 
 protected:
-    void dragEnterEvent(QDragEnterEvent *event);
-    void dragMoveEvent(QDragMoveEvent* event);
-    void dropEvent(QDropEvent *event);
+  QSplitter *vertSplitter;
+  QSplitter *horzSplitter;
+  QSplitter *vertSplitterLeft;
+  QListView *rawFileListView;
+  QListView *vgmFileListView;
+  QListView *vgmCollListView;
+  QListView *collListView;
+
+protected:
+  void dragEnterEvent(QDragEnterEvent *event);
+  void dragMoveEvent(QDragMoveEvent *event);
+  void dropEvent(QDropEvent *event);
 };
-
-#endif // MAINWINDOW_H
-
-
