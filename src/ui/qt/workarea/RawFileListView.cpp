6bb03cf3a67df5a92b0bed4cb0a9b12a6695900f
Eli M <eli@meluc.ci>
2021-06-15 22:02:06 +0200

    VGMTransQt: reimplement RawFileListView

--- src/ui/qt/workarea/RawFileListView.cpp
+++ src/ui/qt/workarea/RawFileListView.cpp
@@ -1,84 +1,181 @@
-//
-// Created by Mike on 8/31/14.
-//
+/*
+ * VGMTrans (c) 2002-2021
+ * Licensed under the zlib license,
+ * refer to the included LICENSE.txt file
+ */
 
 #include <QKeyEvent>
-#include <QStandardItem>
+#include <QMenu>
+#include <QHeaderView>
 #include "RawFileListView.h"
+#include "RawFile.h"
+#include "QtVGMRoot.h"
 
+/*
+ * RawFileListViewModel
+ */
 
-// ********************
-// RawFileListViewModel
-// ********************
-
-RawFileListViewModel::RawFileListViewModel(QObject *parent)
-    : QAbstractListModel(parent)
-{
-    QObject::connect(&qtVGMRoot, SIGNAL(UI_AddedRawFile()), this, SLOT(changedRawFiles()));
-    QObject::connect(&qtVGMRoot, SIGNAL(UI_RemovedRawFile()), this, SLOT(changedRawFiles()));
+RawFileListViewModel::RawFileListViewModel(QObject *parent) : QAbstractTableModel(parent) {
+  connect(&qtVGMRoot, &QtVGMRoot::UI_AddedRawFile, this, &RawFileListViewModel::AddRawFile);
+  connect(&qtVGMRoot, &QtVGMRoot::UI_RemovedRawFile, this, &RawFileListViewModel::RemoveRawFile);
 }
 
-int RawFileListViewModel::rowCount ( const QModelIndex & parent) const
-{
-    return qtVGMRoot.vRawFile.size();
+int RawFileListViewModel::rowCount(const QModelIndex &parent) const {
+  if (parent.isValid())
+    return 0;
+
+  return static_cast<int>(qtVGMRoot.vRawFile.size());
 }
 
-QVariant RawFileListViewModel::data ( const QModelIndex & index, int role ) const
-{
-    if (role == Qt::DisplayRole) {
-        const wchar_t *filename = qtVGMRoot.vRawFile[index.row()]->GetFileName();
-        return QString::fromWCharArray(filename);
-    }
-    else if (role == Qt::DecorationRole) {
-        return QIcon(":/images/file-32.png");
-    }
+int RawFileListViewModel::columnCount(const QModelIndex &parent) const {
+  if (parent.isValid())
+    return 0;
 
+  return 2;
+}
+
+void RawFileListViewModel::AddRawFile() {
+  int position = static_cast<int>(qtVGMRoot.vRawFile.size()) - 1;
+  if (position >= 0) {
+    beginInsertRows(QModelIndex(), position, position);
+    endInsertRows();
+  }
+}
+
+void RawFileListViewModel::RemoveRawFile() {
+  int position = static_cast<int>(qtVGMRoot.vRawFile.size());
+  if (position >= 0) {
+    beginRemoveRows(QModelIndex(), position, position);
+    endRemoveRows();
+  }
+}
+
+QVariant RawFileListViewModel::headerData(int column, Qt::Orientation orientation, int role) const {
+  if (orientation == Qt::Vertical || role != Qt::DisplayRole)
     return QVariant();
-}
 
-QVariant RawFileListViewModel::headerData(int section, Qt::Orientation orientation, int role) const {
-    return "Test Header";
-}
-
-void RawFileListViewModel::changedRawFiles()
-{
-    emit dataChanged(index(0, 0), index(0, 0));
-}
-
-
-// ***************
-// RawFileListView
-// ***************
-
-RawFileListView::RawFileListView(QWidget *parent)
-        : QListView(parent)
-{
-    RawFileListViewModel *rawFileListViewModel = new RawFileListViewModel(this);
-    this->setModel(rawFileListViewModel);
-    this->setSelectionMode(QAbstractItemView::ExtendedSelection);
-    this->setSelectionRectVisible(true);
-}
-
-void RawFileListView::keyPressEvent(QKeyEvent* e)
-{
-    // On Backspace or Delete keypress, remove all selected files
-    if( e->key() == Qt::Key_Delete || e->key() == Qt::Key_Backspace )
-    {
-        QModelIndexList list = this->selectionModel()->selectedIndexes();
-
-        if (list.isEmpty())
-            return;
-
-        QList<RawFile*> filesToClose;
-        foreach(const QModelIndex &index, list) {
-            if (index.row() < qtVGMRoot.vRawFile.size())
-                filesToClose.append(qtVGMRoot.vRawFile[index.row()]);
-        }
-
-
-        foreach(RawFile *file, filesToClose) {
-            printf("In Loop");
-            qtVGMRoot.CloseRawFile(file);
-        }
+  switch (column) {
+    case Property::Name: {
+      return "Name";
     }
+
+    case Property::ContainedFiles: {
+      return "Contained files";
+    }
+
+    default: {
+      return QVariant();
+    }
+  }
+}
+
+QVariant RawFileListViewModel::data(const QModelIndex &index, int role) const {
+  if (!index.isValid()) {
+    return QVariant();
+  }
+
+  switch (index.column()) {
+    case Property::Name: {
+      if (role == Qt::DisplayRole) {
+        return QString::fromWCharArray(qtVGMRoot.vRawFile[index.row()]->GetFileName());
+      } else if (role == Qt::DecorationRole) {
+        static QIcon fileicon(":/images/file.svg");
+        return fileicon;
+      }
+      break;
+    }
+
+    case Property::ContainedFiles: {
+      if (role == Qt::DisplayRole) {
+        return QString::number(qtVGMRoot.vRawFile[index.row()]->containedVGMFiles.size());
+      }
+      break;
+    }
+  }
+
+  return QVariant();
+}
+
+/*
+ * RawFileListView
+ */
+
+RawFileListView::RawFileListView(QWidget *parent) : QTableView(parent) {
+  rawFileListViewModel = new RawFileListViewModel(this);
+  setModel(rawFileListViewModel);
+  setAlternatingRowColors(true);
+  setShowGrid(false);
+  setWordWrap(false);
+  setIconSize(QSize(16, 16));
+
+  setSelectionMode(QAbstractItemView::ExtendedSelection);
+  setSelectionBehavior(QAbstractItemView::SelectRows);
+
+  verticalHeader()->hide();
+  auto header_hor = horizontalHeader();
+  header_hor->setSectionsMovable(true);
+  header_hor->setHighlightSections(true);
+  header_hor->setSectionResizeMode(QHeaderView::Stretch);
+
+  setContextMenuPolicy(Qt::CustomContextMenu);
+  rawfile_context_menu = new QMenu();
+  QAction *rawfile_remove = rawfile_context_menu->addAction("Remove");
+  connect(rawfile_remove, &QAction::triggered, this, &RawFileListView::DeleteRawFiles);
+  rawfile_context_menu->addAction("Save raw unpacked image(s)", [sm = selectionModel()]() {
+    if (!sm->hasSelection())
+      return;
+
+    QModelIndexList list = sm->selectedRows();
+    for (auto &index : list) {
+      auto rawfile = qtVGMRoot.vRawFile[index.row()];
+      std::wstring filepath = pRoot->UI_GetSaveFilePath(L"");
+      if (!filepath.empty()) {
+        /* todo: free function in VGMExport */
+        rawfile->OnSaveAsRaw();
+      }
+    }
+  });
+
+  connect(this, &QAbstractItemView::customContextMenuRequested, this,
+          &RawFileListView::RawFilesMenu);
+}
+
+/*
+ * This is different from the other context menus,
+ * since the only possible action on a RawFile is removing it
+ */
+void RawFileListView::RawFilesMenu(const QPoint &pos) {
+  if (!indexAt(pos).isValid())
+    return;
+
+  rawfile_context_menu->exec(mapToGlobal(pos));
+}
+
+void RawFileListView::keyPressEvent(QKeyEvent *input) {
+  // On Backspace or Delete keypress, remove all selected files
+  switch (input->key()) {
+    case Qt::Key_Delete:
+    case Qt::Key_Backspace: {
+      DeleteRawFiles();
+    }
+
+    // Pass the event back to the base class, needed for keyboard navigation
+    default:
+      QTableView::keyPressEvent(input);
+  }
+}
+
+void RawFileListView::DeleteRawFiles() {
+  if (!selectionModel()->hasSelection())
+    return;
+
+  QModelIndexList list = selectionModel()->selectedRows();
+  std::vector<RawFile *> to_close;
+  to_close.reserve(list.size());
+  for (auto &index : list) {
+    to_close.emplace_back(qtVGMRoot.vRawFile[index.row()]);
+  }
+
+  std::for_each(std::begin(to_close), std::end(to_close),
+                [](RawFile *file) { qtVGMRoot.CloseRawFile(file); });
 }
