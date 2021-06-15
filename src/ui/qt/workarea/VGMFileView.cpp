122218410fe9c4de6771768a83d583a677f988f1
Eli M <eli@meluc.ci>
2021-06-15 17:32:18 +0200

    VGMTransQt: VGMFileView: use qhexview, implement item descriptions, delete old hexview implementation

--- src/ui/qt/workarea/VGMFileView.cpp
+++ src/ui/qt/workarea/VGMFileView.cpp
@@ -1,47 +1,90 @@
+/*
+ * VGMTrans (c) 2002-2021
+ * Licensed under the zlib license,
+ * refer to the included LICENSE.txt file
+ */
+
 #include "VGMFileView.h"
-#include "VGMFile.h"
-#include "HexView.h"
+#include <QShortcut>
+#include <QFontDatabase>
+#include <VGMFile.h>
+#include <qhexview.h>
+#include <document/buffer/qmemoryrefbuffer.h>
 #include "VGMFileTreeView.h"
 #include "MdiArea.h"
-#include "../util/Helpers.h"
-
+#include "Helpers.h"
 
 const int splitterHandleWidth = 1;
 
+VGMFileView::VGMFileView(VGMFile *vgmfile)
+    : QSplitter(Qt::Horizontal), m_vgmfile(vgmfile), m_hexview(new QHexView) {
+  /* this copy is nasty, removing it involves some serious backend refactoring... */
+  auto &internal_buffer = m_buffer.buffer();
+  internal_buffer.resize(m_vgmfile->unLength);
+  m_vgmfile->GetBytes(m_vgmfile->dwOffset, m_vgmfile->unLength, internal_buffer.data());
+  m_buffer.open(QIODevice::ReadOnly);
 
-VGMFileView::VGMFileView(VGMFile *vgmFile)
-        : QSplitter(Qt::Horizontal, 0)
-{
-    hexView = new HexView(vgmFile, this);
-    treeView = new VGMFileTreeView(vgmFile, this);
+  auto document = QHexDocument::fromDevice<QMemoryRefBuffer>(&m_buffer);
+  document->setBaseAddress(m_vgmfile->dwOffset);
 
-    this->addWidget(hexView);
-    this->addWidget(treeView);
+  m_hexview->setDocument(document);
+  m_hexview->setReadOnly(true);
+  QFont fixedFont = QFontDatabase::systemFont(QFontDatabase::FixedFont);
+  fixedFont.setPointSizeF(10.5);
+  m_hexview->setFont(fixedFont);
+  markEvents();
 
-//    horzSplitter->setMaximumSize(500, 0);
-//    horzSplitter->setCollapsible(0, false);
-//    horzSplitter->setCollapsible(1, false);
-//    qDebug( "%d   %d  %d", parent->size().width(), this->size().width(), this->maximumSize().width() );
+  setWindowTitle(QString::fromStdWString(*m_vgmfile->GetName()));
+  setWindowIcon(iconForFileType(m_vgmfile->GetFileType()));
+  setAttribute(Qt::WA_DeleteOnClose);
 
-//    QList<int> sizes = QList<int>({100, 30});
+  m_treeview = new VGMFileTreeView(m_vgmfile, this);
 
-    this->setSizes(QList<int>() << 500 << 100);
-//    this->setMinimumSize(700, 100);
+  addWidget(m_hexview);
+  addWidget(m_treeview);
+  setSizes(QList<int>() << 900 << 270);
 
-    this->setStretchFactor(1, 1);
-//    this->setSizes(sizes);
-    this->setHandleWidth(splitterHandleWidth);
+  new QShortcut(QKeySequence::ZoomIn, this, [hexview = m_hexview]() {
+    auto font = hexview->font();
+    font.setPointSizeF(font.pointSizeF() + 0.5);
+    hexview->setFont(font);
+  });
 
-    QString vgmFileName = QString::fromStdWString(*vgmFile->GetName());
-    setWindowTitle(vgmFileName);
-    setWindowIcon(iconForFileType(vgmFile->GetFileType()));
+  new QShortcut(QKeySequence::ZoomOut, this, [hexview = m_hexview]() {
+    auto font = hexview->font();
+    font.setPointSizeF(font.pointSizeF() - 0.5);
+    hexview->setFont(font);
+  });
 }
 
+void VGMFileView::markEvents() {
+  auto base_offset = m_vgmfile->dwOffset;
+  auto overlay = m_hexview->document()->metadata();
+  uint32_t i = 0;
+  while (i < m_vgmfile->unLength) {
+    auto item = m_vgmfile->GetItemFromOffset(base_offset + i, false);
+    if (item) {
+      auto line = std::floor((item->dwOffset - base_offset) / 16);
+      auto col = (item->dwOffset - base_offset) % 16;
+      auto item_len = item->unLength;
+      auto desc = QString::fromStdWString(item->GetDescription());
+      while (col + item_len > 16) {
+        auto part_len = 16 - col;
+        overlay->metadata(line, col, part_len, textColorForEventColor(item->color),
+                          colorForEventColor(item->color), desc);
+        line++;
+        col = 0;
+        item_len -= part_len;
+      }
+      overlay->metadata(line, col, item_len, textColorForEventColor(item->color),
+                        colorForEventColor(item->color), desc);
+      i += item->unLength;
+    } else {
+      i++;
+    }
+  }
+}
 void VGMFileView::addToMdi() {
-    MdiArea::getInstance()->addSubWindow(this);
-    show();
-}
-
-VGMFileView::~VGMFileView()
-{
+  MdiArea::getInstance()->addSubWindow(this);
+  show();
 }
--- src/ui/qt/workarea/VGMFileView.h
+++ src/ui/qt/workarea/VGMFileView.h
@@ -1,28 +1,32 @@
-#ifndef VGMTRANS_BREAKDOWNVIEW_H
-#define VGMTRANS_BREAKDOWNVIEW_H
+/*
+ * VGMTrans (c) 2002-2021
+ * Licensed under the zlib license,
+ * refer to the included LICENSE.txt file
+ */
 
+#pragma once
 #include <QMainWindow>
-#include <qsplitter.h>
+#include <QSplitter>
+#include <QBuffer>
 
 class VGMFile;
 class HexView;
+class QHexView;
 class VGMFileTreeView;
 
-class VGMFileView : public QSplitter {
-    Q_OBJECT
+class VGMFileView final : public QSplitter {
+  Q_OBJECT
 
 public:
-    VGMFileView(VGMFile *vgmFile);
-    ~VGMFileView();
+  explicit VGMFileView(VGMFile *vgmFile);
 
-    void addToMdi();
+  void addToMdi();
 
+private:
+  void markEvents();
 
-protected:
-    QSplitter *horzSplitter;
-    HexView *hexView;
-    VGMFileTreeView *treeView;
+  VGMFileTreeView *m_treeview{};
+  VGMFile *m_vgmfile{};
+  QHexView *m_hexview{};
+  QBuffer m_buffer{};
 };
-
-
-#endif //VGMTRANS_BREAKDOWNVIEW_H
