49f9f30b9503c3401c9d561145d19e1031085a59
Eli M <eli@meluc.ci>
2021-06-20 15:13:13 +0200

    VGMTransQt: implement icon bar

---
+++ src/ui/qt/IconBar.h
@@ -0,0 +1,41 @@
+/*
+ * VGMTrans (c) 2002-2021
+ * Licensed under the zlib license,
+ * refer to the included LICENSE.txt file
+ */
+
+#pragma once
+
+#include <QToolBar>
+#include <QIcon>
+
+class QSlider;
+class QAction;
+class QLabel;
+
+class IconBar final : public QToolBar {
+    Q_OBJECT
+public:
+  explicit IconBar(QWidget *parent = nullptr);
+
+signals:
+  void openPressed();
+  void playToggle();
+  void stopPressed();
+  void seekingTo(int position);
+
+private slots:
+  void playerStatusChanged(bool playing);
+  void playbackRangeUpdate(int cur, int max);
+
+private:
+  void setupControls();
+
+  QAction *m_open{};
+  QAction *m_play{};
+  QAction *m_stop{};
+  QSlider *m_slider{};
+  QLabel *m_title;
+  inline static QIcon s_playicon;
+  inline static QIcon s_pauseicon;
+};
