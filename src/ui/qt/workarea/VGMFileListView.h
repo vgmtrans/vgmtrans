84749059b7aa49b82b8daabaa6d47512754db6ef
Eli M <eli@meluc.ci>
2021-06-20 17:48:57 +0200

    VGMTransQt: VGMFileListView: drop type column
    item icon is self-explanatory

--- src/ui/qt/workarea/VGMFileListView.h
+++ src/ui/qt/workarea/VGMFileListView.h
@@ -29,7 +29,7 @@ class VGMFileListModel : public QAbstractTableModel {
     void RemoveVGMFile();
 
    private:
-    enum Property : uint8_t { Name = 0, Type = 1, Format = 2 };
+    enum Property : uint8_t { Name = 0, Format = 1 };
 };
 
 class VGMFileListView final : public QTableView {
