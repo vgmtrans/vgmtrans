5978b8e826aa8706be98cb64d413748f9fbd8c3d
Eli M <eli@meluc.ci>
2021-06-20 13:26:12 +0200

    delete common, move MusicPlayer under VGMTransQt
    "premature generalization is the root of all evil". since WTL will be dropped and no other GUIs are planned in the foreseeable future, it makes sense to move this and get rid of common entirely: there isn't anything that warrants being shared

--- src/ui/qt/MenuBar.cpp
+++ src/ui/qt/MenuBar.cpp
@@ -33,12 +33,12 @@ void MenuBar::AppendOptionsMenu() {
   auto audio_backend = options_dropdown->addMenu("Player audio driver");
   menu_drivers = new QActionGroup(this);
 
-  for (auto &driver : common::MusicPlayer::the().getAvailableDrivers()) {
+  for (auto &driver : MusicPlayer::the().getAvailableDrivers()) {
     auto driveropt = audio_backend->addAction(QString(driver));
     menu_drivers->addAction(driveropt);
 
     driveropt->setCheckable(true);
-    if (common::MusicPlayer::the().checkSetting("audio.driver", driver)) {
+    if (MusicPlayer::the().checkSetting("audio.driver", driver)) {
       driveropt->setChecked(true);
       auto font = driveropt->font();
       font.setBold(true);
@@ -47,7 +47,7 @@ void MenuBar::AppendOptionsMenu() {
   }
 
   connect(menu_drivers, &QActionGroup::triggered, [](QAction *driver) {
-    common::MusicPlayer::the().setAudioDriver(driver->text().toStdString().c_str());
+    MusicPlayer::the().setAudioDriver(driver->text().toStdString().c_str());
   });
 
   menu_logger_show = options_dropdown->addAction("Show logs");
