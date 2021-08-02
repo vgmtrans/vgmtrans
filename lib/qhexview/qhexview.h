1b4833d341aa987af196a8e2ac3df3e81928473f
DubiousDoggo <jft360@gmail.com>
2021-08-02 03:42:17 -0500

    VGMTransQt: Remove hexrenderer, merge with hexview

--- lib/qhexview/qhexview.h
+++ lib/qhexview/qhexview.h
@@ -5,7 +5,8 @@
 
 #include <QAbstractScrollArea>
 #include <QTimer>
-#include "document/qhexrenderer.h"
+#include <QTextDocument>
+#include <QTextCursor>
 #include "document/qhexdocument.h"
 
 class QHexView : public QAbstractScrollArea
@@ -14,21 +15,21 @@ class QHexView : public QAbstractScrollArea
 
     public:
         explicit QHexView(QWidget *parent = nullptr);
-        QHexDocument* document();
+        QHexDocument* document() { return m_document; };
         void setDocument(QHexDocument* document);
         void setReadOnly(bool b);
 
     protected:
-        virtual bool event(QEvent* e);
-        virtual void keyPressEvent(QKeyEvent *e);
-        virtual void mousePressEvent(QMouseEvent* e);
-        virtual void mouseMoveEvent(QMouseEvent* e);
-        virtual void mouseReleaseEvent(QMouseEvent* e);
-        virtual void focusInEvent(QFocusEvent* e);
-        virtual void focusOutEvent(QFocusEvent* e);
-        virtual void wheelEvent(QWheelEvent* e);
-        virtual void resizeEvent(QResizeEvent* e);
-        virtual void paintEvent(QPaintEvent* e);
+        virtual bool event(QEvent* e) override;
+        virtual void keyPressEvent(QKeyEvent *e) override;
+        virtual void mousePressEvent(QMouseEvent* e) override;
+        virtual void mouseMoveEvent(QMouseEvent* e) override;
+        virtual void mouseReleaseEvent(QMouseEvent* e) override;
+        virtual void focusInEvent(QFocusEvent* e) override;
+        virtual void focusOutEvent(QFocusEvent* e) override;
+        virtual void wheelEvent(QWheelEvent* e) override;
+        virtual void resizeEvent(QResizeEvent* e) override;
+        virtual void paintEvent(QPaintEvent* e) override;
 
     private slots:
         void renderCurrentLine();
@@ -56,9 +57,63 @@ class QHexView : public QAbstractScrollArea
 
     private:
         QHexDocument* m_document;
-        QHexRenderer* m_renderer;
         QTimer* m_blinktimer;
         bool m_readonly;
+
+    // renderer
+    public:
+        enum RenderArea { HEADER_AREA, ADDRESS_AREA, HEX_AREA, ASCII_AREA, EXTRA_AREA };
+
+    public:
+        void renderFrame(QPainter* painter);
+        void render(QPainter* painter, quint64 start, quint64 end, quint64 firstline);  // begin included, end excluded
+        void updateMetrics(const QFontMetricsF& fm);
+        void enableCursor(bool b = true);
+        void selectArea(const QPoint& pt);
+
+    public:
+        bool hitTest(const QPoint& pt, QHexPosition* position, quint64 firstline) const;
+        RenderArea hitTestArea(const QPoint& pt) const;
+        int selectedArea() const;
+        bool editableArea(int area) const;
+        quint64 documentLastLine() const;
+        int documentLastColumn() const;
+        quint64 documentLines() const;
+        int documentWidth() const;
+        int lineHeight() const;
+        QRect getLineRect(quint64 line, quint64 firstline) const;
+        int headerLineCount() const { return 1; }
+        int borderSize() const;
+        int hexLineWidth() const;
+
+    private:
+        QString hexString(quint64 line, QByteArray *rawline = nullptr) const;
+        QString asciiString(quint64 line, QByteArray *rawline = nullptr) const;
+        QByteArray getLine(quint64 line) const;
+        qint64 rendererLength() const;
+        int getAddressWidth() const;
+        int getHexColumnX() const;
+        int getAsciiColumnX() const;
+        int getEndColumnX() const;
+        qreal getCellWidth() const;
+        int getNCellsWidth(int n) const;
+        void unprintableChars(QByteArray &ascii) const;
+
+    private:
+        void applyDocumentStyles(QPainter* painter, QTextDocument *textdocument) const;
+        void applyBasicStyle(QTextCursor& textcursor, const QByteArray& rawline, int factor = 1) const;
+        void applyMetadata(QTextCursor& textcursor, quint64 line, int factor = 1) const;
+        void applySelection(QTextCursor& textcursor, quint64 line, int factor = 1) const;
+        void applyCursorAscii(QTextCursor& textcursor, quint64 line) const;
+        void applyCursorHex(QTextCursor& textcursor, quint64 line) const;
+        void drawAddress(QPainter *painter, const QPalette &palette, const QRect &linerect, quint64 line);
+        void drawHex(QPainter *painter, const QPalette &palette, const QRect &linerect, quint64 line);
+        void drawAscii(QPainter *painter, const QPalette &palette, const QRect &linerect, quint64 line);
+        void drawHeader(QPainter *painter, const QPalette &palette);
+
+    private:
+        int m_selectedarea;
+        bool m_cursorenabled;
 };
 
 #endif // QHEXVIEW_H
