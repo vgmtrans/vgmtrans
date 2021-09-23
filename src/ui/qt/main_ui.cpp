629864282c3116a492b3ec0a9df6811930b04341
Eli M <eli@meluc.ci>
2021-09-23 10:58:07 +0200

    VGMTransQt: MusicPlayer: make the audio driver selection persistent across application launches

--- src/ui/qt/main_ui.cpp
+++ src/ui/qt/main_ui.cpp
@@ -18,6 +18,9 @@ int main(int argc, char *argv[]) {
   QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #endif
 
+  QCoreApplication::setOrganizationName("VGMTrans");
+  QCoreApplication::setApplicationName("VGMTrans");
+
   QApplication app(argc, argv);
   qtVGMRoot.Init();
 
