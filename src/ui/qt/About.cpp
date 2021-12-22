bb9360304699809cdeaf292ce0cf22ace67b8342
E. Melucci <eli@meluc.ci>
2021-12-22 16:20:04 +0100

    VGMTransQt: About: make window a modal, reformat
    Signed-off-by: E. Melucci <eli@meluc.ci>

--- src/ui/qt/About.cpp
+++ src/ui/qt/About.cpp
@@ -10,13 +10,15 @@
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QPixmap>
+#include <QPushButton>
 #include <version.h>
 
 About::About(QWidget *parent) : QDialog(parent) {
-    setWindowTitle("About VGMTrans");
-    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+  setWindowTitle("About VGMTrans");
+  setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint & Qt::Sheet);
+  setWindowModality(Qt::WindowModal);
 
-    auto text = R"(
+  auto text = R"(
         <p style='font-size:36pt; font-weight:300; margin-bottom:0;'>VGMTrans</p>
         <p style='margin-top:0;'>Version: <b>%1 (%2, %3)</b></p>
         <hr>
@@ -24,30 +26,38 @@ About::About(QWidget *parent) : QDialog(parent) {
         <a href='https://github.com/vgmtrans/vgmtrans'>Source code</a>
     )";
 
-    QLabel *text_label = new QLabel(QString(text).arg(VGMTRANS_VERSION).arg(VGMTRANS_REVISION).arg(VGMTRANS_BRANCH));
-    text_label->setTextInteractionFlags(Qt::TextBrowserInteraction);
-    text_label->setOpenExternalLinks(true);
-    text_label->setContentsMargins(15, 0, 15, 0);
+  QLabel *text_label =
+      new QLabel(QString(text).arg(VGMTRANS_VERSION).arg(VGMTRANS_REVISION).arg(VGMTRANS_BRANCH));
+  text_label->setTextInteractionFlags(Qt::TextBrowserInteraction);
+  text_label->setOpenExternalLinks(true);
+  text_label->setContentsMargins(15, 0, 15, 0);
 
-    QLabel *copyright =
-        new QLabel("<p style='margin-top:0; margin-bottom:0; font-size:small;'>&copy; 2002-2021 VGMTrans Team | Licensed under the zlib license<br>SoundFont&reg; is a registered trademark of Creative Technology Ltd.</p>");
+  QLabel *copyright =
+      new QLabel("<p style='margin-top:0; margin-bottom:0; font-size:small;'>&copy; 2002-2021 "
+                 "VGMTrans Team | Licensed under the zlib license<br>SoundFont&reg; is a "
+                 "registered trademark of Creative Technology Ltd.</p>");
 
-    QLabel *logo = new QLabel();
-    logo->setPixmap(QPixmap(":/vgmtrans.png").scaledToHeight(250, Qt::SmoothTransformation));
-    logo->setContentsMargins(15, 0, 15, 0);
+  QLabel *logo = new QLabel();
+  logo->setPixmap(QPixmap(":/vgmtrans.png").scaledToHeight(250, Qt::SmoothTransformation));
+  logo->setContentsMargins(15, 0, 15, 0);
 
-    QVBoxLayout *main_layout = new QVBoxLayout;
-    QHBoxLayout *h_layout = new QHBoxLayout;
+  QVBoxLayout *main_layout = new QVBoxLayout;
+  QHBoxLayout *h_layout = new QHBoxLayout;
 
-    setLayout(main_layout);
-    main_layout->addLayout(h_layout);
-    main_layout->addWidget(copyright);
-    copyright->setAlignment(Qt::AlignCenter);
-    copyright->setContentsMargins(0, 15, 0, 0);
+  auto close_btn = new QPushButton("Close");
+  connect(close_btn, &QPushButton::pressed, this, &QDialog::accept);
 
-    h_layout->setAlignment(Qt::AlignLeft);
-    h_layout->addWidget(logo);
-    h_layout->addWidget(text_label);
-    
-    layout()->setSizeConstraint(QLayout::SetFixedSize);
+  setLayout(main_layout);
+  main_layout->addLayout(h_layout);
+  main_layout->addWidget(copyright);
+  main_layout->addWidget(close_btn);
+
+  copyright->setAlignment(Qt::AlignCenter);
+  copyright->setContentsMargins(0, 15, 0, 0);
+
+  h_layout->setAlignment(Qt::AlignLeft);
+  h_layout->addWidget(logo);
+  h_layout->addWidget(text_label);
+
+  layout()->setSizeConstraint(QLayout::SetFixedSize);
 }
