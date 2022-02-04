8ef66a4e862cf3a2a904d17954f06e17f2e23746
E. Melucci <eli@meluc.ci>
2022-02-04 10:56:05 +0100

    LogItem, LogListView: remove DateTime
    while seemingly a nice to have we have no real use for it and it slows things down. the datetime code also yields incorrect results on some platforms
    
    Signed-off-by: E. Melucci <eli@meluc.ci>

--- src/main/LogItem.cpp
+++ src/main/LogItem.cpp
@@ -1,31 +1,11 @@
 #include "pch.h"
 
-#include "datetime.h"
-
 #include "LogItem.h"
-
-LogItem::LogItem() :
-    text(),
-    time(DateTime::get_Now()),
-    level(LOG_LEVEL_ERR),
-    source() {
-}
-
-LogItem::LogItem(const wchar_t *text, LogLevel level, const wchar_t *source) :
-    text(text ? text : L""),
-    time(DateTime::get_Now()),
-    level(level),
-    source(source ? source : L"") {
-}
 
 LogItem::LogItem(const std::wstring &text, LogLevel level, const std::wstring &source) :
     text(text),
-    time(DateTime::get_Now()),
-    level(level),
-    source(source) {
-}
-
-LogItem::~LogItem() {
+    source(source),
+    level(level) {
 }
 
 std::wstring LogItem::GetText() const {
@@ -34,10 +14,6 @@ std::wstring LogItem::GetText() const {
 
 const wchar_t *LogItem::GetCText() const {
   return text.c_str();
-}
-
-DateTime LogItem::GetTime() const {
-  return time;
 }
 
 LogLevel LogItem::GetLogLevel() const {
--- src/main/LogItem.h
+++ src/main/LogItem.h
@@ -1,20 +1,15 @@
 #pragma once
 
 #include <string>
-#include "datetime.h"
 
 enum LogLevel { LOG_LEVEL_ERR, LOG_LEVEL_WARN, LOG_LEVEL_INFO, LOG_LEVEL_DEBUG };
 
 class LogItem {
  public:
-  LogItem();
-  LogItem(const wchar_t *text, LogLevel level, const wchar_t *source);
   LogItem(const std::wstring &text, LogLevel level, const std::wstring &source);
-  virtual ~LogItem();
 
   std::wstring GetText() const;
   const wchar_t *GetCText() const;
-  DateTime GetTime() const;
   LogLevel GetLogLevel() const;
   std::wstring GetSource() const;
   const wchar_t *GetCSource() const;
@@ -22,6 +17,5 @@ class LogItem {
  protected:
   std::wstring text;
   std::wstring source;
-  DateTime time;
   LogLevel level;
 };
