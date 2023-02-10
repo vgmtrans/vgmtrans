7a119bf1e5f57118608f2af07b13742dec60913b
Jeremy Silver <jeremys@nessiness.com>
2023-02-09 21:28:22 -0500

    CLI: Add CMakeLists and main function

---
+++ src/ui/cli/main_cli.cpp
@@ -0,0 +1,15 @@
+/**
+ * VGMTrans (c) - 2002-2021
+ * Licensed under the zlib license
+ * See the included LICENSE for more information
+ */
+
+#include "pch.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+  std::cout << "Hello, world!" << std::endl;
+
+}
