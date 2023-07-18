vgmtrans/vgmtrans

commit 01a29156ca61e7f28c3a7592bd9e8df1a09c7d47

Add SysexEvent class and update CPS portamento handling (#374)
* * Add SysexEvent class to simplify defining sysex midi events
* Update Reset events to use SysexEvent
* Add PortamentoTimeMode event for usage in upcoming VST playback
* Add portamentoTimeMode property to VGMSeq
* Improve portamento implementation again in CPS format
* delete midi in VGMSeq destructor

* Remove CPS format force legato notes for portamento hack

* BASS Audio Library seems to insist on reading the first data byte of a sysex event as data length, even though this is not a MIDI standard (it should read until an 0xF7).

To avoid breaking BASS, we'll exclude the new Portamento Time Mode Event. We've already made accomodations around all of the MIDI reset events.

src/main/MidiFile.h
@@ -11,6 +11,7 @@ class MidiTrack;
 class MidiEvent;
 class DurNoteEvent;
 class NoteEvent;
+enum class PortamentoTimeMode: uint8_t;
 
 #define PRIORITY_LOWEST 127
 #define PRIORITY_LOWER 96
@@ -46,6 +47,7 @@ typedef enum {
   MIDIEVENT_PORTAMENTO,
   MIDIEVENT_PORTAMENTOTIME,
   MIDIEVENT_PORTAMENTOTIMEFINE,
+  MIDIEVENT_PORTAMENTOTIMEMODE,
   MIDIEVENT_PORTAMENTOCONTROL,
   MIDIEVENT_MONO,
   MIDIEVENT_LFO,
@@ -113,6 +115,7 @@ class MidiTrack {
   void InsertPortamentoTime(uint8_t channel, uint8_t time, uint32_t absTime);
   void AddPortamentoTimeFine(uint8_t channel, uint8_t time);
   void InsertPortamentoTimeFine(uint8_t channel, uint8_t time, uint32_t absTime);
+  void AddPortamentoTimeMode(PortamentoTimeMode mode);
   void AddPortamentoControl(uint8_t channel, uint8_t key);
   void AddMono(uint8_t channel);
   void InsertMono(uint8_t channel, uint32_t absTime);
@@ -231,7 +234,6 @@ class MidiEvent {
   void WriteVarLength(std::vector<uint8_t> &buf, uint32_t value);
   //virtual void PrepareWrite(void/*vector<MidiEvent*> & aEvents*/);
   virtual uint32_t WriteEvent(std::vector<uint8_t> &buf, uint32_t time) = 0;
-  uint32_t WriteSysexEvent(std::vector<uint8_t> &buf, uint32_t time, uint8_t *data, size_t dataSize);
   uint32_t WriteMetaEvent(std::vector<uint8_t> &buf, uint32_t time, uint8_t metaType, uint8_t *data, size_t dataSize);
   uint32_t WriteMetaTextEvent(std::vector<uint8_t> &buf, uint32_t time, uint8_t metaType, std::wstring wstr);
 
@@ -304,6 +306,19 @@ class ControllerEvent
   uint8_t dataByte;
 };
 
+class SysexEvent
+    : public MidiEvent {
+public:
+  SysexEvent(MidiTrack *prntTrk,
+             uint32_t absoluteTime,
+             vector<uint8_t> sysexData,
+             int8_t thePriority = PRIORITY_MIDDLE);
+  virtual MidiEventType GetEventType() { return MIDIEVENT_UNDEFINED; }
+  virtual uint32_t WriteEvent(std::vector<uint8_t> &buf, uint32_t time);
+
+  vector<uint8_t> sysexData;
+};
+
 class VolumeEvent
     : public ControllerEvent {
  public:
@@ -350,6 +365,14 @@ public:
   PortamentoTimeFineEvent(MidiTrack *prntTrk, uint8_t channel, uint32_t absoluteTime, uint8_t time)
       : ControllerEvent(prntTrk, channel, absoluteTime, 37, time, PRIORITY_MIDDLE) { }
   virtual MidiEventType GetEventType() { return MIDIEVENT_PORTAMENTOTIMEFINE; }
+};
+
+class PortamentoTimeModeEvent
+    : public SysexEvent {
+public:
+  PortamentoTimeModeEvent(MidiTrack *prntTrk, uint32_t absoluteTime, PortamentoTimeMode mode)
+      : SysexEvent(prntTrk, absoluteTime, {0x7D, 0x7E, static_cast<uint8_t>(mode) }, PRIORITY_MIDDLE) { }
+  virtual MidiEventType GetEventType() { return MIDIEVENT_PORTAMENTOTIMEMODE; }
 };
 
 class PortamentoControlEvent
@@ -423,16 +446,14 @@ public:
 	uint8_t vol;
 };*/
 
-class MastVolEvent
-    : public MidiEvent {
- public:
-  MastVolEvent(MidiTrack *prntTrk, uint8_t channel, uint32_t absoluteTime, uint8_t mastVol);
+class MasterVolEvent
+    : public SysexEvent {
+public:
+  MasterVolEvent(MidiTrack *prntTrk, uint8_t channel, uint32_t absoluteTime, uint8_t msb)
+      : SysexEvent(prntTrk, absoluteTime, {0x7F, 0x7F, 0x04, 0x01, 0, static_cast<uint8_t>(msb) }, PRIORITY_HIGHER) { }
   virtual MidiEventType GetEventType() { return MIDIEVENT_MASTERVOL; }
-  //virtual MastVolEvent* MakeCopy();
-  virtual uint32_t WriteEvent(std::vector<uint8_t> &buf, uint32_t time);
-
-  uint8_t mastVol;
 };
+
 /*
 class ExpressionEvent
 	: public MidiEvent
@@ -581,35 +602,37 @@ class MarkerEvent
 };
 
 class GMResetEvent
-    : public MidiEvent {
+    : public SysexEvent {
  public:
-  GMResetEvent(MidiTrack *prntTrk, uint32_t absoluteTime);
+  GMResetEvent(MidiTrack *prntTrk, uint32_t absoluteTime)
+       : SysexEvent(prntTrk, absoluteTime, { 0x05, 0x7E, 0x7F, 0x09, 0x01 }, PRIORITY_HIGHEST) { }
   virtual MidiEventType GetEventType() { return MIDIEVENT_RESET; }
-  virtual uint32_t WriteEvent(std::vector<uint8_t> &buf, uint32_t time);
 };
 
 class GM2ResetEvent
-    : public MidiEvent {
+    : public SysexEvent {
  public:
-  GM2ResetEvent(MidiTrack *prntTrk, uint32_t absoluteTime);
+  GM2ResetEvent(MidiTrack *prntTrk, uint32_t absoluteTime)
+       : SysexEvent(prntTrk, absoluteTime, { 0x05, 0x7E, 0x7F, 0x09, 0x03 }, PRIORITY_HIGHEST) { }
   virtual MidiEventType GetEventType() { return MIDIEVENT_RESET; }
-  virtual uint32_t WriteEvent(std::vector<uint8_t> &buf, uint32_t time);
 };
 
 class GSResetEvent
-    : public MidiEvent {
+    : public SysexEvent {
  public:
-  GSResetEvent(MidiTrack *prntTrk, uint32_t absoluteTime);
+  GSResetEvent(MidiTrack *prntTrk, uint32_t absoluteTime)
+       : SysexEvent(prntTrk, absoluteTime, { 0x0A, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41 },
+                    PRIORITY_HIGHEST) { }
   virtual MidiEventType GetEventType() { return MIDIEVENT_RESET; }
-  virtual uint32_t WriteEvent(std::vector<uint8_t> &buf, uint32_t time);
 };
 
 class XGResetEvent
-    : public MidiEvent {
+    : public SysexEvent {
  public:
-  XGResetEvent(MidiTrack *prntTrk, uint32_t absoluteTime);
+  XGResetEvent(MidiTrack *prntTrk, uint32_t absoluteTime)
+       : SysexEvent(prntTrk, absoluteTime,  { 0x08, 0x43, 0x10, 0x4C, 0x00, 0x00, 0x7E, 0x00 },
+                    PRIORITY_HIGHEST) { }
   virtual MidiEventType GetEventType() { return MIDIEVENT_RESET; }
-  virtual uint32_t WriteEvent(std::vector<uint8_t> &buf, uint32_t time);
 };
 
 

src/main/MidiFile.cpp
@@ -306,14 +306,15 @@ void MidiTrack::InsertVol(uint8_t channel, uint8_t vol, uint32_t absTime) {
   aEvents.push_back(new VolumeEvent(this, channel, absTime, vol));
 }
 
-//mast volume has a full byte of resolution
+//TODO: Master Volume sysex events are meant to be global to device, not per channel.
+// For per channel master volume, we should add a system for normalizing controller vol events.
 void MidiTrack::AddMasterVol(uint8_t channel, uint8_t mastVol) {
-  MidiEvent *newEvent = new MastVolEvent(this, channel, GetDelta(), mastVol);
+  MidiEvent *newEvent = new MasterVolEvent(this, channel, GetDelta(), mastVol);
   aEvents.push_back(newEvent);
 }
 
 void MidiTrack::InsertMasterVol(uint8_t channel, uint8_t mastVol, uint32_t absTime) {
-  MidiEvent *newEvent = new MastVolEvent(this, channel, absTime, mastVol);
+  MidiEvent *newEvent = new MasterVolEvent(this, channel, absTime, mastVol);
   aEvents.push_back(newEvent);
 }
 
@@ -355,6 +356,10 @@ void MidiTrack::AddPortamentoTimeFine(uint8_t channel, uint8_t time) {
 
 void MidiTrack::InsertPortamentoTimeFine(uint8_t channel, uint8_t time, uint32_t absTime) {
   aEvents.push_back(new PortamentoTimeFineEvent(this, channel, absTime, time));
+}
+
+void MidiTrack::AddPortamentoTimeMode(PortamentoTimeMode mode) {
+  aEvents.push_back(new PortamentoTimeModeEvent(this, GetDelta(), mode));
 }
 
 void MidiTrack::AddPortamentoControl(uint8_t channel, uint8_t key) {
@@ -577,6 +582,38 @@ void MidiTrack::InsertTrackName(const std::wstring &wstr, uint32_t absTime) {
   aEvents.push_back(new TrackNameEvent(this, absTime, wstr));
 }
 
+void MidiTrack::AddGMReset() {
+  aEvents.push_back(new GMResetEvent(this, GetDelta()));
+}
+
+void MidiTrack::InsertGMReset(uint32_t absTime) {
+  aEvents.push_back(new GMResetEvent(this, absTime));
+}
+
+void MidiTrack::AddGM2Reset() {
+  aEvents.push_back(new GM2ResetEvent(this, GetDelta()));
+}
+
+void MidiTrack::InsertGM2Reset(uint32_t absTime) {
+  aEvents.push_back(new GM2ResetEvent(this, absTime));
+}
+
+void MidiTrack::AddGSReset() {
+  aEvents.push_back(new GSResetEvent(this, GetDelta()));
+}
+
+void MidiTrack::InsertGSReset(uint32_t absTime) {
+  aEvents.push_back(new GSResetEvent(this, absTime));
+}
+
+void MidiTrack::AddXGReset() {
+  aEvents.push_back(new XGResetEvent(this, GetDelta()));
+}
+
+void MidiTrack::InsertXGReset(uint32_t absTime) {
+  aEvents.push_back(new XGResetEvent(this, absTime));
+}
+
 // SPECIAL NON-MIDI EVENTS
 
 // Transpose events offset the key when we write the Midi file.
@@ -625,7 +662,8 @@ bool MidiEvent::IsMetaEvent() {
 bool MidiEvent::IsSysexEvent() {
   MidiEventType type = GetEventType();
   return type == MIDIEVENT_MASTERVOL ||
-         type == MIDIEVENT_RESET;
+         type == MIDIEVENT_RESET ||
+         type == MIDIEVENT_PORTAMENTOTIMEMODE;
 }
 
 void MidiEvent::WriteVarLength(vector<uint8_t> &buf, uint32_t value) {
@@ -644,16 +682,6 @@ void MidiEvent::WriteVarLength(vector<uint8_t> &buf, uint32_t value) {
     else
       break;
   }
-}
-
-uint32_t MidiEvent::WriteSysexEvent(vector<uint8_t> &buf, uint32_t time, uint8_t *data, size_t dataSize) {
-  WriteVarLength(buf, AbsTime - time);
-  buf.push_back(0xF0);
-  WriteVarLength(buf, (uint32_t) dataSize);
-  for (size_t dataIndex = 0; dataIndex < dataSize; dataIndex++) {
-    buf.push_back(data[dataIndex]);
-  }
-  return AbsTime;
 }
 
 uint32_t MidiEvent::WriteMetaEvent(vector<uint8_t> &buf,
@@ -783,20 +811,6 @@ VolEvent* VolEvent::MakeCopy()
 	return new VolEvent(prntTrk, channel, AbsTime, vol, priority);
 }*/
 
-
-//  ************
-//  MastVolEvent
-//  ************
-
-MastVolEvent::MastVolEvent(MidiTrack *prntTrk, uint8_t channel, uint32_t absoluteTime, uint8_t theMastVol)
-    : MidiEvent(prntTrk, absoluteTime, channel, PRIORITY_HIGHER), mastVol(theMastVol) {
-}
-
-uint32_t MastVolEvent::WriteEvent(vector<uint8_t> &buf, uint32_t time) {
-  uint8_t data[7] = {0x7F, 0x7F, 0x04, 0x01, /*LSB*/0, (uint8_t) (mastVol & 0x7F), 0xF7};
-  return WriteSysexEvent(buf, time, data, 7);
-}
-
 //  ***************
 //  ControllerEvent
 //  ***************
@@ -815,6 +829,25 @@ uint32_t ControllerEvent::WriteEvent(vector<uint8_t> &buf, uint32_t time) {
   buf.push_back(0xB0 + channel);
   buf.push_back(controlNum & 0x7F);
   buf.push_back(dataByte);
+  return AbsTime;
+}
+
+//  **********
+//  SysexEvent
+//  **********
+
+SysexEvent::SysexEvent(MidiTrack *prntTrk,
+                       uint32_t absoluteTime,
+                       vector<uint8_t> sysexData,
+                       int8_t thePriority)
+    : MidiEvent(prntTrk, absoluteTime, 0, thePriority), sysexData(sysexData) {
+}
+
+uint32_t SysexEvent::WriteEvent(vector<uint8_t> &buf, uint32_t time) {
+  WriteVarLength(buf, AbsTime - time);
+  buf.push_back(0xF0);
+  buf.insert(buf.end(), sysexData.begin(), sysexData.end());
+  buf.push_back(0xF7);
   return AbsTime;
 }
 
@@ -955,90 +988,6 @@ TrackNameEvent::TrackNameEvent(MidiTrack *prntTrk, uint32_t absoluteTime, const 
 
 uint32_t TrackNameEvent::WriteEvent(vector<uint8_t> &buf, uint32_t time) {
   return WriteMetaTextEvent(buf, time, 0x03, text);
-}
-
-//  ************
-//  GMResetEvent
-//  ************
-
-GMResetEvent::GMResetEvent(MidiTrack *prntTrk, uint32_t absoluteTime)
-    : MidiEvent(prntTrk, absoluteTime, 0, PRIORITY_HIGHEST) {
-}
-
-uint32_t GMResetEvent::WriteEvent(vector<uint8_t> &buf, uint32_t time) {
-  uint8_t data[5] = {0x7E, 0x7F, 0x09, 0x01, 0xF7};
-  return WriteSysexEvent(buf, time, data, 5);
-}
-
-void MidiTrack::AddGMReset() {
-  aEvents.push_back(new GMResetEvent(this, GetDelta()));
-}
-
-void MidiTrack::InsertGMReset(uint32_t absTime) {
-  aEvents.push_back(new GMResetEvent(this, absTime));
-}
-
-//  *************
-//  GM2ResetEvent
-//  *************
-
-GM2ResetEvent::GM2ResetEvent(MidiTrack *prntTrk, uint32_t absoluteTime)
-    : MidiEvent(prntTrk, absoluteTime, 0, PRIORITY_HIGHEST) {
-}
-
-uint32_t GM2ResetEvent::WriteEvent(vector<uint8_t> &buf, uint32_t time) {
-  uint8_t data[5] = {0x7E, 0x7F, 0x09, 0x03, 0xF7};
-  return WriteSysexEvent(buf, time, data, 5);
-}
-
-void MidiTrack::AddGM2Reset() {
-  aEvents.push_back(new GM2ResetEvent(this, GetDelta()));
-}
-
-void MidiTrack::InsertGM2Reset(uint32_t absTime) {
-  aEvents.push_back(new GM2ResetEvent(this, absTime));
-}
-
-//  ************
-//  GSResetEvent
-//  ************
-
-GSResetEvent::GSResetEvent(MidiTrack *prntTrk, uint32_t absoluteTime)
-    : MidiEvent(prntTrk, absoluteTime, 0, PRIORITY_HIGHEST) {
-}
-
-uint32_t GSResetEvent::WriteEvent(vector<uint8_t> &buf, uint32_t time) {
-  uint8_t data[10] = {0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7};
-  return WriteSysexEvent(buf, time, data, 10);
-}
-
-void MidiTrack::AddGSReset() {
-  aEvents.push_back(new GSResetEvent(this, GetDelta()));
-}
-
-void MidiTrack::InsertGSReset(uint32_t absTime) {
-  aEvents.push_back(new GSResetEvent(this, absTime));
-}
-
-//  ************
-//  XGResetEvent
-//  ************
-
-XGResetEvent::XGResetEvent(MidiTrack *prntTrk, uint32_t absoluteTime)
-    : MidiEvent(prntTrk, absoluteTime, 0, PRIORITY_HIGHEST) {
-}
-
-uint32_t XGResetEvent::WriteEvent(vector<uint8_t> &buf, uint32_t time) {
-  uint8_t data[8] = {0x43, 0x10, 0x4C, 0x00, 0x00, 0x7E, 0x00, 0xF7};
-  return WriteSysexEvent(buf, time, data, 8);
-}
-
-void MidiTrack::AddXGReset() {
-  aEvents.push_back(new XGResetEvent(this, GetDelta()));
-}
-
-void MidiTrack::InsertXGReset(uint32_t absTime) {
-  aEvents.push_back(new XGResetEvent(this, absTime));
 }
 
 //***************

src/main/SeqTrack.cpp
@@ -166,6 +166,8 @@ void SeqTrack::AddInitialMidiEvents(int trackNum) {
     pMidiTrack->AddGM2Reset();
     if (parentSeq->bAlwaysWriteInitialTempo)
       pMidiTrack->AddTempoBPM(parentSeq->initialTempoBPM);
+    if (parentSeq->portamentoTimeMode != PortamentoTimeMode::kUndefined)
+      AddPortamentoTimeModeNoItem(parentSeq->portamentoTimeMode);
   }
   if (parentSeq->bAlwaysWriteInitialVol)
     AddVolNoItem(parentSeq->initialVol);
@@ -1152,6 +1154,11 @@ void SeqTrack::InsertPortamentoTime(uint32_t offset,
 void SeqTrack::InsertPortamentoTimeNoItem(uint8_t time, uint32_t absTime) {
   if (readMode == READMODE_CONVERT_TO_MIDI)
     pMidiTrack->InsertPortamentoTime(channel, time, absTime);
+}
+
+void SeqTrack::AddPortamentoTimeModeNoItem(PortamentoTimeMode mode) {
+  if (readMode == READMODE_CONVERT_TO_MIDI)
+    pMidiTrack->AddPortamentoTimeMode(mode);
 }
 
 void SeqTrack::AddPortamentoControlNoItem(uint8_t key) {

src/main/SeqTrack.h
@@ -123,6 +123,7 @@ class SeqTrack:
   void InsertPortamentoTimeNoItem(uint8_t time, uint32_t absTime);
   void AddPortamentoTime14Bit(uint32_t offset, uint32_t length, uint16_t time, const std::wstring &sEventName = L"Portamento Time");
   void AddPortamentoTime14BitNoItem(uint16_t time);
+  void AddPortamentoTimeModeNoItem(PortamentoTimeMode mode);
   void AddPortamentoControlNoItem(uint8_t key);
   void AddProgramChange(uint32_t offset, uint32_t length, uint32_t progNum, const std::wstring &sEventName = L"Program Change");
   void AddProgramChange(uint32_t offset, uint32_t length, uint32_t progNum, uint8_t chan, const std::wstring &sEventName = L"Program Change");

src/main/VGMSeq.cpp
@@ -26,6 +26,7 @@ VGMSeq::VGMSeq(const string &format, RawFile *file, uint32_t offset, uint32_t le
       bAlwaysWriteInitialPitchBendRange(false),
       bAlwaysWriteInitialMono(false),
       bAllowDiscontinuousTrackData(false),
+      portamentoTimeMode(PortamentoTimeMode::kUndefined),
       bLoadTickByTick(false),
       bIncTickAfterProcessingTracks(true),
       initialVol(100),                    //GM standard (dls1 spec p16)
@@ -43,6 +44,7 @@ VGMSeq::VGMSeq(const string &format, RawFile *file, uint32_t offset, uint32_t le
 VGMSeq::~VGMSeq(void) {
   DeleteVect<SeqTrack>(aTracks);
   DeleteVect<ISeqSlider>(aSliders);
+  delete midi;
 }
 
 bool VGMSeq::Load() {

src/main/VGMSeq.h
@@ -20,6 +20,12 @@ enum class PanVolumeCorrectionMode : uint8_t {
   kAdjustExpressionController
 };
 
+enum class PortamentoTimeMode : uint8_t {
+  kDurationInMs = 0,
+  kCentsPerSecond,
+  kUndefined
+};
+
 class VGMSeq: public VGMFile {
  public:
   BEGIN_MENU_SUB(VGMSeq, VGMFile)
@@ -80,6 +86,10 @@ class VGMSeq: public VGMFile {
     bAlwaysWriteInitialMono = true;
   }
 
+  void AlwaysWritePortamentoTimeMode(PortamentoTimeMode mode) {
+    portamentoTimeMode = mode;
+  }
+
   bool OnSaveAsMidi(void);
   virtual bool SaveAsMidi(const std::wstring &filepath);
 
@@ -115,6 +125,7 @@ class VGMSeq: public VGMFile {
   bool bAlwaysWriteInitialPitchBendRange;
   bool bAlwaysWriteInitialMono;
   bool bAllowDiscontinuousTrackData;
+  PortamentoTimeMode portamentoTimeMode;
 
   // True if each tracks in a sequence needs to be loaded simultaneously in tick by tick, as the real music player does.
   // Pros:

src/main/formats/CPSSeq.cpp
@@ -19,6 +19,9 @@ CPSSeq::CPSSeq(RawFile *file, uint32_t offset, CPSFormatVer fmtVersion, wstring 
   HasMonophonicTracks();
   AlwaysWriteInitialVol(127);
   AlwaysWriteInitialMonoMode();
+  // While we still use the BASS audio library, we won't call the PortamentoTimeMode Sysex event
+  // because BASS insists the first sysex data byte be the length of the data, which is non-standard
+//  AlwaysWritePortamentoTimeMode(PortamentoTimeMode::kCentsPerSecond);
 }
 
 CPSSeq::~CPSSeq(void) {

src/main/formats/CPSTrackV1.cpp
@@ -65,7 +65,7 @@ bool CPSTrackV1::ReadEvent(void) {
         }
         else if (key != prevTieNote) {
           AddNoteOn(beginOffset, curOffset - beginOffset, key, 127, L"Note On (tied)");
-          InsertNoteOffNoItem(prevTieNote, GetTime()+2);
+          AddNoteOffNoItem(prevTieNote);
         }
         else
           AddGenericEvent(beginOffset, curOffset - beginOffset, L"Tie", L"", CLR_NOTEON);
@@ -74,7 +74,6 @@ bool CPSTrackV1::ReadEvent(void) {
       }
       else {
         if (bPrevNoteTie) {
-          AddPortamentoNoItem(false);
           if (key != prevTieNote) {
             AddNoteOffNoItem(prevTieNote);
             AddNoteByDur(beginOffset, curOffset - beginOffset, key, 127, absDur);
@@ -84,6 +83,7 @@ bool CPSTrackV1::ReadEvent(void) {
             delta -= absDur;
             AddNoteOff(beginOffset, curOffset - beginOffset, prevTieNote, L"Note Off (tied)");
           }
+          AddPortamentoNoItem(false);
         }
         else {
           AddNoteByDur(beginOffset, curOffset - beginOffset, key, 127, absDur);

commit a5a0967a064725f44e171faa811b1e059352dcc7

MidiFile: Add missing size byte to MasterVolEvent
This fixes a regression in commit 01a2915 where SNES music is missing instruments
at the right times (I used Corneria theme from Star Fox for my testing)

src/main/MidiFile.h
@@ -450,7 +450,7 @@ class MasterVolEvent
     : public SysexEvent {
 public:
   MasterVolEvent(MidiTrack *prntTrk, uint8_t channel, uint32_t absoluteTime, uint8_t msb)
-      : SysexEvent(prntTrk, absoluteTime, {0x7F, 0x7F, 0x04, 0x01, 0, static_cast<uint8_t>(msb) }, PRIORITY_HIGHER) { }
+      : SysexEvent(prntTrk, absoluteTime, {0x07, 0x7F, 0x7F, 0x04, 0x01, 0, static_cast<uint8_t>(msb) }, PRIORITY_HIGHER) { }
   virtual MidiEventType GetEventType() { return MIDIEVENT_MASTERVOL; }
 };
 