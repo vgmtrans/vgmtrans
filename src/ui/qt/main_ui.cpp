8ca41360278756a6067d10cb5fd349e7bf4801fb
mikelow <mikelow@users.noreply.github.com>
2023-08-12 14:38:17 -0700

    remove high dpi scaling and pixmaps for Qt version < 6

--- src/ui/qt/main_ui.cpp
+++ src/ui/qt/main_ui.cpp
@@ -13,11 +13,6 @@
 #include "QtVGMRoot.h"
 
 int main(int argc, char *argv[]) {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-  QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-#endif
-
   QCoreApplication::setOrganizationName("VGMTrans");
   QCoreApplication::setApplicationName("VGMTrans");
 
