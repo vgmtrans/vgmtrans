e18171ee836ba35c1af2374968294bd814ae9a12
mikelow <mikelow@users.noreply.github.com>
2023-12-12 20:11:16 -0800

    * improve hexview / filetreeview splitter behavior * ensure increasing/decreasing hex view font size has actual effect

--- src/ui/qt/workarea/HexView.cpp
+++ src/ui/qt/workarea/HexView.cpp
@@ -33,9 +33,9 @@ HexView::HexView(VGMFile* vgmfile, QWidget *parent) :
   lineCache.setMaxCost(NUM_CACHED_LINE_PIXMAPS);
 
   double appFontPointSize = QApplication::font().pointSizeF();
-  QFont font("Roboto Mono", appFontPointSize + 2);
+  QFont font("Roboto Mono", appFontPointSize);
   // Call setPointSizeF, as QFont() doesn't accept a float size value
-  font.setPointSizeF(appFontPointSize + 2.0);
+  font.setPointSizeF(appFontPointSize);
 
   this->setFont(font);
   this->setFocusPolicy(Qt::StrongFocus);
