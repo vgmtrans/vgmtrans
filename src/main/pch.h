640af33124b346e356af82f370baefafea585b9d
Echo J <tcg96nougat@gmail.com>
2024-04-29 18:14:07 +0300

    Remove countof fallbacks
    This project now targets C++17 by default (so the fallbacks have
    no use anymore)

--- src/main/pch.h
+++ src/main/pch.h
@@ -28,20 +28,10 @@
 #include <ctype.h>
 
 
-// countof definition with fallbacks. Taken from: http://www.g-truc.net/post-0708.html
-//  TODO: replace this with ibkt the simple C++11 definition when VS2015 is released
-
-// Any compiler claiming C++11 supports, Visual C++ 2015 and Clang version supporting constexp
-#if __cplusplus >= 201103L || _MSC_VER >= 1900 // C++ 11 implementation
+// C++11 countof definition taken from http://www.g-truc.net/post-0708.html
 template<typename T, std::size_t N>
 constexpr std::size_t countof(T const (&)[N]) noexcept {
   return N;
 }
-
-#elif _MSC_VER // Visual C++ fallback
-  #define countof(arr) _countof(arr)
-#else
-  #define countof(arr) sizeof(arr) / sizeof(arr[0])
-#endif
 
 using namespace std;
