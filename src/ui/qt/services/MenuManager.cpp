49dd47a1814fad45ceed8b874dadc950b02af850
mikelow <mikelow@users.noreply.github.com>
2024-05-29 12:42:56 -0700

    Remove "Close" option from VGMColl Menu

--- src/ui/qt/services/MenuManager.cpp
+++ src/ui/qt/services/MenuManager.cpp
@@ -47,8 +47,6 @@ MenuManager::MenuManager() {
       std::make_shared<SaveCollCommand<conversion::Target::MIDI | conversion::Target::DLS>>(),
       std::make_shared<SaveCollCommand<conversion::Target::MIDI | conversion::Target::SF2
                        | conversion::Target::DLS>>(),
-      std::make_shared<CommandSeparator>(),
-      std::make_shared<CloseRawFileCommand>(),
   });
 }
 
