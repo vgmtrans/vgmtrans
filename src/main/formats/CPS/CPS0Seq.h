9f0e3c4bdca8581615f4f1c3a8e08862c7bbf822
mikelow <mikelow@users.noreply.github.com>
2024-10-16 23:29:41 -0700

    initial work on early CPS1 driver

---
+++ src/main/formats/CPS/CPS0Seq.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "VGMSeq.h"
+
+class CPS0Seq:
+    public VGMSeq {
+public:
+  CPS0Seq(RawFile *file, uint32_t offset, std::string name = {}, std::vector<s8> instrTransposeTable = {});
+  ~CPS0Seq() = default;
+
+  bool parseHeader() override;
+  bool parseTrackPointers() override;
+  s8 getTransposeForInstr(u8 instrIndex);
+  u8 masterVolume() { return masterVol; }
+  void setMasterVolume(u8 vol) { masterVol = vol; }
+
+public:
+  u16 tempoRaw = 1;
+
+private:
+  std::vector<s8> instrTransposeTable;
+  uint8_t masterVol = 0x7F;
+};
