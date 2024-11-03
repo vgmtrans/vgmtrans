bb7e3520b57042d233c57c5722ea86b46d1c3388
mikelow <mikelow@users.noreply.github.com>
2024-11-03 01:22:54 -0800

    - add K054539AdpcmSamp class - cleanup KonamiArcadeSeq logic

--- src/main/formats/KonamiArcade/KonamiArcadeSeq.cpp
+++ src/main/formats/KonamiArcade/KonamiArcadeSeq.cpp
@@ -58,28 +58,20 @@ bool KonamiArcadeTrack::readEvent() {
   uint32_t beginOffset = curOffset;
   uint8_t status_byte = readByte(curOffset++);
 
-  if (status_byte == 0xFF) {
-    if (bInJump) {
-      bInJump = false;
-      curOffset = jump_return_offset;
-    }
-    else {
-      addEndOfTrack(beginOffset, curOffset - beginOffset);
-      return 0;
-    }
-  }
-  else if (status_byte == 0x60) {
+  if (status_byte == 0x60) {
     addUnknown(beginOffset, curOffset - beginOffset);
-    return 1;
+    return true;
   }
-  else if (status_byte == 0x61) {
+
+  if (status_byte == 0x61) {
     addUnknown(beginOffset, curOffset - beginOffset);
-    return 1;
+    return true;
   }
-  else if (status_byte < 0xC0)    //note event
-  {
+
+  // Note
+  if (status_byte < 0xC0) {
     uint8_t note, delta;
-    if (status_byte < 0x62) {
+    if (status_byte < 0x60) {
       delta = readByte(curOffset++);
       note = status_byte;
       prevDelta = delta;
@@ -107,285 +99,296 @@ bool KonamiArcadeTrack::readEvent() {
       newdur = 1;
     addNoteByDur(beginOffset, curOffset - beginOffset, note, vel, newdur);
     addTime(delta);
+    return true;
   }
-  else {
-    int loopNum;
-    switch (status_byte) {
-      case 0xC0:
-        curOffset++;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
 
-      case 0xC2:
-      case 0xC3:
-        curOffset++;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
+  int loopNum;
+  switch (status_byte) {
+    case 0xC0:
+      curOffset++;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
 
-      case 0xCE:
-        curOffset += 2;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
+    case 0xC2:
+    case 0xC3:
+      curOffset++;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
 
-      case 0xD0:
-        curOffset += 3;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
+    case 0xCE:
+      curOffset += 2;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
 
-      case 0xD1:
-      case 0xD2:
-      case 0xD3:
-      case 0xD4:
-      case 0xD5:
-      case 0xD6:
-        curOffset += 2;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
+    case 0xD0:
+      curOffset += 3;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
 
-      case 0xD7:
-        curOffset += 3;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
+    case 0xD1:
+    case 0xD2:
+    case 0xD3:
+    case 0xD4:
+    case 0xD5:
+    case 0xD6:
+      curOffset += 2;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
 
-      case 0xD8:
-        curOffset += 2;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
+    case 0xD7:
+      curOffset += 3;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
 
-      case 0xD9:
-        curOffset += 3;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
+    case 0xD8:
+      curOffset += 2;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
 
-      case 0xDA:
-        curOffset += 2;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
+    case 0xD9:
+      curOffset += 3;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
 
-      case 0xDE:
-        curOffset++;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
+    case 0xDA:
+      curOffset += 2;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
 
-      // Rest
-      case 0xE0: {
-        uint8_t delta = readByte(curOffset++);
-        // addTime(delta);
-        addRest(beginOffset, curOffset-beginOffset, delta);
-        break;
-      }
+    case 0xDE:
+      curOffset++;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
 
-      // Hold
-      case 0xE1: {
-        uint8_t delta = readByte(curOffset++);
-        uint8_t dur = readByte(curOffset++);
-        uint32_t newdur = (delta * dur) / 0x64;
-        addTime(newdur);
-        this->makePrevDurNoteEnd();
-        addTime(delta - newdur);
-        auto desc = fmt::format("total delta: {:d} ticks  additional duration: {:d} ticks", delta, newdur);
-        addGenericEvent(beginOffset, curOffset - beginOffset, "Hold", desc, CLR_TIE);
-        break;
-      }
+    // Rest
+    case 0xE0: {
+      uint8_t delta = readByte(curOffset++);
+      // addTime(delta);
+      addRest(beginOffset, curOffset-beginOffset, delta);
+      break;
+    }
 
-      // program change
-      case 0xE2: {
-        uint8_t progNum = readByte(curOffset++);
-        addProgramChange(beginOffset, curOffset - beginOffset, progNum);
-        break;
-      }
+    // Hold
+    case 0xE1: {
+      uint8_t delta = readByte(curOffset++);
+      uint8_t dur = readByte(curOffset++);
+      uint32_t newdur = (delta * dur) / 0x64;
+      addTime(newdur);
+      this->makePrevDurNoteEnd();
+      addTime(delta - newdur);
+      auto desc = fmt::format("total delta: {:d} ticks  additional duration: {:d} ticks", delta, newdur);
+      addGenericEvent(beginOffset, curOffset - beginOffset, "Hold", desc, CLR_TIE);
+      break;
+    }
 
-      // Pan
-      case 0xE3:
-        // 0x00 seems centered.
-        // 0x01 causes full left,
-        // 0x7F causes full right, channel
-        // but collapses to center very quickly. 0x05 sounds centered, as does 0x7F - 4
-        curOffset++;
-        addUnknown(beginOffset, curOffset - beginOffset, "Pan");
-        break;
+    // program change
+    case 0xE2: {
+      uint8_t progNum = readByte(curOffset++);
+      addProgramChange(beginOffset, curOffset - beginOffset, progNum);
+      break;
+    }
 
-      // LFO settings (or perhaps just vibrato)
-      case 0xE4: {
-        u8 depth = readByte(curOffset++); // lower seems to result in more effect. 7F turns off all vibrato regardless of amplitude.
-        u8 freq = readByte(curOffset++);  // higher results in faster fluctuation
-        u8 amplitude = readByte(curOffset++); // higher results in greater LFO effect
-        addUnknown(beginOffset, curOffset - beginOffset, "Vibrato?");
-        break;
-      }
+    // Pan
+    case 0xE3:
+      // 0x00 seems centered.
+      // 0x01 causes full left,
+      // 0x7F causes full right, channel
+      // but collapses to center very quickly. 0x05 sounds centered, as does 0x7F - 4
+      curOffset++;
+      addUnknown(beginOffset, curOffset - beginOffset, "Pan");
+      break;
 
-      case 0xE5:
-        curOffset += 3;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
+    // LFO settings (or perhaps just vibrato)
+    case 0xE4: {
+      u8 depth = readByte(curOffset++); // lower seems to result in more effect. 7F turns off all vibrato regardless of amplitude.
+      u8 freq = readByte(curOffset++);  // higher results in faster fluctuation
+      u8 amplitude = readByte(curOffset++); // higher results in greater LFO effect
+      addUnknown(beginOffset, curOffset - beginOffset, "Vibrato?");
+      break;
+    }
 
-      case 0xE6:
-        loopNum = 0;
-        goto loopMarker;
-      case 0xE8: {
-        loopNum = 1;
-      loopMarker:
-        loopMarker[loopNum] = curOffset;
-        auto name = fmt::format("Loop {:d} Marker", loopNum);
-        addGenericEvent(beginOffset, curOffset - beginOffset, name, "", CLR_LOOP);
-        break;
-      }
+    case 0xE5:
+      curOffset += 3;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
 
-      case 0xE7:
-        loopNum = 0;
-        goto doLoop;
-      case 0xE9: {
-        loopNum = 1;
-      doLoop:
-        u8 loopCount = readByte(curOffset++);
-        u8 initialLoopAtten = -readByte(curOffset++);
-        u8 initialLoopTranspose = readByte(curOffset++);
-        if (loopCounter[loopNum] == 0) {
-          if (loopCount == 1) {
-            loopAtten[loopNum] = 0;
-            loopTranspose[loopNum] = 0;
-            break;
-          }
-          loopCounter[loopNum] = loopCount - 1;
-          loopAtten[loopNum] = initialLoopAtten;
-          loopTranspose[loopNum] = initialLoopTranspose;
+    case 0xE6:
+      loopNum = 0;
+      goto loopMarker;
+    case 0xE8: {
+      loopNum = 1;
+    loopMarker:
+      loopMarker[loopNum] = curOffset;
+      auto name = fmt::format("Loop {:d} Marker", loopNum);
+      addGenericEvent(beginOffset, curOffset - beginOffset, name, "", CLR_LOOP);
+      break;
+    }
+
+    case 0xE7:
+      loopNum = 0;
+      goto doLoop;
+    case 0xE9: {
+      loopNum = 1;
+    doLoop:
+      u8 loopCount = readByte(curOffset++);
+      u8 initialLoopAtten = -readByte(curOffset++);
+      u8 initialLoopTranspose = readByte(curOffset++);
+      if (loopCounter[loopNum] == 0) {
+        if (loopCount == 1) {
+          loopAtten[loopNum] = 0;
+          loopTranspose[loopNum] = 0;
+          break;
         }
-        else {
-          if (loopCount > 0 && --loopCounter[loopNum] == 0) {
-            loopAtten[loopNum] = 0;
-            loopTranspose[loopNum] = 0;
-            break;
-          }
-          loopAtten[loopNum] += initialLoopAtten;
-          loopTranspose[loopNum] += initialLoopTranspose;
+        loopCounter[loopNum] = loopCount - 1;
+        loopAtten[loopNum] = initialLoopAtten;
+        loopTranspose[loopNum] = initialLoopTranspose;
+      }
+      else {
+        if (loopCount > 0 && --loopCounter[loopNum] == 0) {
+          loopAtten[loopNum] = 0;
+          loopTranspose[loopNum] = 0;
+          break;
         }
-
-        // TODO: confusing logic sets a couple channel state vars here based on atten and transpose
-
-        bool shouldContinue = true;
-        if (loopCount == 0) {
-          shouldContinue = addLoopForever(beginOffset, curOffset - beginOffset);
-        }
-        else {
-          auto desc = fmt::format("count: {:d}  attenuation: {:d}  transpose {:d}", loopCount,
-            initialLoopAtten, initialLoopTranspose);
-          addGenericEvent(beginOffset, curOffset - beginOffset, "Loop", desc, CLR_LOOP);
-        }
-        if (loopMarker[loopNum] < parentSeq->dwOffset) {
-          L_ERROR("KonamiArcadeSeq wants to loopMarker outside bounds of sequence. Loop at %X", beginOffset);
-          return false;
-        }
-        printf("E7/E9 LOOPING FROM %X", beginOffset);
-        curOffset = loopMarker[loopNum];
-        printf(" TO: %X\n", curOffset);
-        return shouldContinue;
+        loopAtten[loopNum] += initialLoopAtten;
+        loopTranspose[loopNum] += initialLoopTranspose;
       }
 
-      //tempo
-      case 0xEA: {
-        u8 bpm = readByte(curOffset++);
-        addTempoBPM(beginOffset, curOffset - beginOffset, bpm);
-        break;
+      // TODO: confusing logic sets a couple channel state vars here based on atten and transpose
+
+      bool shouldContinue = true;
+      if (loopCount == 0) {
+        shouldContinue = addLoopForever(beginOffset, curOffset - beginOffset);
       }
-
-      case 0xEC:
-        curOffset++;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
-
-      // Volume
-      case 0xEE: {
-        u8 vol = readByte(curOffset++);
-        u8 atten = ~vol & 0x7F;
-        addVol(beginOffset, curOffset - beginOffset, vol);
-        //AddMasterVol(beginOffset, curOffset-beginOffset, vol);
-        break;
+      else {
+        auto desc = fmt::format("count: {:d}  attenuation: {:d}  transpose {:d}", loopCount,
+          initialLoopAtten, initialLoopTranspose);
+        addGenericEvent(beginOffset, curOffset - beginOffset, "Loop", desc, CLR_LOOP);
       }
-
-      case 0xEF:
-        curOffset += 2;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
-
-      case 0xF0:
-        curOffset++;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
-
-      case 0xF1:
-        curOffset += 3;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
-
-      case 0xF2:
-        curOffset++;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
-
-      case 0xF3:
-        curOffset += 3;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
-
-      case 0xF6:
-      case 0xF7:
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
-
-      case 0xF8:
-        curOffset += 2;
-        addUnknown(beginOffset, curOffset - beginOffset);
-        break;
-
-      case 0xF9: {
-        u8 data = readByte(curOffset++);
-        addUnknown(beginOffset, curOffset - beginOffset);
+      if (loopMarker[loopNum] < parentSeq->dwOffset) {
+        L_ERROR("KonamiArcadeSeq wants to loopMarker outside bounds of sequence. Loop at %X", beginOffset);
+        return false;
       }
+      printf("E7/E9 LOOPING FROM %X", beginOffset);
+      curOffset = loopMarker[loopNum];
+      printf(" TO: %X\n", curOffset);
+      return shouldContinue;
+    }
 
-      //release rate
-      case 0xFA: {
-        // lower value results in longer sustain
-        u8 releaseRate = readByte(curOffset++);
-        addUnknown(beginOffset, curOffset - beginOffset, "Release Rate");
-        break;
+    //tempo
+    case 0xEA: {
+      u8 bpm = readByte(curOffset++);
+      addTempoBPM(beginOffset, curOffset - beginOffset, bpm);
+      break;
+    }
+
+    case 0xEC:
+      curOffset++;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
+
+    // Volume
+    case 0xEE: {
+      u8 vol = readByte(curOffset++);
+      u8 atten = ~vol & 0x7F;
+      addVol(beginOffset, curOffset - beginOffset, vol);
+      //AddMasterVol(beginOffset, curOffset-beginOffset, vol);
+      break;
+    }
+
+    case 0xEF:
+      curOffset += 2;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
+
+    case 0xF0:
+      curOffset++;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
+
+    case 0xF1:
+      curOffset += 3;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
+
+    case 0xF2:
+      curOffset++;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
+
+    case 0xF3:
+      curOffset += 3;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
+
+    case 0xF6:
+    case 0xF7:
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
+
+    case 0xF8:
+      curOffset += 2;
+      addUnknown(beginOffset, curOffset - beginOffset);
+      break;
+
+    case 0xF9: {
+      u8 data = readByte(curOffset++);
+      addUnknown(beginOffset, curOffset - beginOffset);
+    }
+
+    //release rate
+    case 0xFA: {
+      // lower value results in longer sustain
+      u8 releaseRate = readByte(curOffset++);
+      addUnknown(beginOffset, curOffset - beginOffset, "Release Rate");
+      break;
+    }
+
+    case 0xFD: {
+      auto seq = static_cast<KonamiArcadeSeq*>(parentSeq);
+      u16 memJumpOffset = readShort(curOffset);
+      u32 romOffset = seq->dwOffset + (memJumpOffset - seq->memOffset());
+      bool shouldContinue = addLoopForever(beginOffset, 3);
+      if (romOffset < seq->dwOffset) {
+        L_ERROR("KonamiArcadeEvent FD attempted jump to offset outside the sequence at {:X}.  Jump offset: {:X}", beginOffset, romOffset);
+        return false;
       }
+      printf("FD LOOPING FROM %X", beginOffset);
+      curOffset = romOffset;
+      printf(" TO: %X\n", curOffset);
+      return shouldContinue;
+    }
 
-      case 0xFD: {
-        auto seq = static_cast<KonamiArcadeSeq*>(parentSeq);
-        u16 memJumpOffset = readShort(curOffset);
-        u32 romOffset = seq->dwOffset + (memJumpOffset - seq->memOffset());
-        bool shouldContinue = addLoopForever(beginOffset, 3);
-        if (romOffset < seq->dwOffset) {
-          L_ERROR("KonamiArcadeEvent FD attempted jump to offset outside the sequence at {:X}.  Jump offset: {:X}", beginOffset, romOffset);
-          return false;
-        }
-        printf("FD LOOPING FROM %X", beginOffset);
-        curOffset = romOffset;
-        printf(" TO: %X\n", curOffset);
-        return shouldContinue;
+    case 0xFE: {
+      bInJump = true;
+      jump_return_offset = curOffset + 2;
+      addGenericEvent(beginOffset, jump_return_offset - beginOffset, "Jump", "", CLR_LOOP);
+      auto seq = static_cast<KonamiArcadeSeq*>(parentSeq);
+      u16 memJumpOffset = readShort(curOffset);
+      u32 romOffset = seq->dwOffset + (memJumpOffset - seq->memOffset());
+      if (romOffset < seq->dwOffset) {
+        L_ERROR("KonamiArcade Event FE attempted jump to offset outside the sequence at {:X}.  Jump offset: {:X}", beginOffset, romOffset);
+        return false;
       }
-
-      case 0xFE: {
-        bInJump = true;
-        jump_return_offset = curOffset + 2;
-        addGenericEvent(beginOffset, jump_return_offset - beginOffset, "Jump", "", CLR_LOOP);
-        auto seq = static_cast<KonamiArcadeSeq*>(parentSeq);
-        u16 memJumpOffset = readShort(curOffset);
-        u32 romOffset = seq->dwOffset + (memJumpOffset - seq->memOffset());
-        if (romOffset < seq->dwOffset) {
-          L_ERROR("KonamiArcade Event FE attempted jump to offset outside the sequence at {:X}.  Jump offset: {:X}", beginOffset, romOffset);
-          return false;
-        }
-        printf("FE LOOPING FROM %X", beginOffset);
-        curOffset = romOffset;
-        printf(" TO: %X\n", curOffset);
-        break;
+      printf("FE LOOPING FROM %X", beginOffset);
+      curOffset = romOffset;
+      printf(" TO: %X\n", curOffset);
+      break;
+    }
+    case 0xFF: {
+      if (bInJump) {
+        bInJump = false;
+        curOffset = jump_return_offset;
       }
-
-      default:
+      else {
         addEndOfTrack(beginOffset, curOffset - beginOffset);
         return false;
+      }
+      break;
     }
+
+    default:
+      addEndOfTrack(beginOffset, curOffset - beginOffset);
+      return false;
   }
   return true;
 }
