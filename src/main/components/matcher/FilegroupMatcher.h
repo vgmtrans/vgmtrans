8c33267b7bc4bd3da99c09da511a28e4b38263ec
mikelow <mikelow@users.noreply.github.com>
2025-05-08 18:33:40 -0700

    - rework FilegroupMatcher so that it's more nuanced - add VGMInstrSet::isViableSampCollMatch() so that instrument sets can help filter sample collection matches - update Vab so that it retains the vag size list and uses it to validate viable sample collections

--- src/main/components/matcher/FilegroupMatcher.h
+++ src/main/components/matcher/FilegroupMatcher.h
@@ -17,13 +17,25 @@ class VGMSampColl;
 // FilegroupMatcher
 // ****************
 
-// FilegroupMatcher handles formats where the only method of associating sequences, instrument sets,
-// and sample collections is that they are loaded together within the same RawFile. When loading is
-// complete, FilegroupMatcher processes the VGMFiles in the order they were added and groups them
-// into collections: thus, it assumes association by order. FilegroupMatcher does not retain
-// state between loads, so it will never create a collection that spans multiple RawFiles, with the
-// exception that FilegroupMatcher processes a psf file and all of its psflib dependencies together
-// as a single load.
+// FilegroupMatcher handles formats where the primary method of associating sequences, instrument
+// sets, and sample collections is that they are loaded together within the same RawFile. When
+// loading is complete, FilegroupMatcher processes the VGMFiles in the order they were added and
+// groups them into collections: thus, it assumes association by order. FilegroupMatcher does not
+// retain state between loads, so it will never create a collection that spans multiple RawFiles,
+// with the exception that FilegroupMatcher processes a psf file and all of its psflib dependencies
+// together as a single load.
+
+// When loading is complete, FilegroupMatcher:
+// 1) Sorts sequences, instrument sets, and sample collections by their offsets.
+// 2) Attempts to pair each instrument set with a sample collection. It calls
+// VGMInstrSet::isViableSampCollMatch() to filter out non-viable sample collections. If the file
+// contains a single instrument set and sample collection, they are always paired. After pairing, a
+// sample collection is removed from the pool for matching, unless it is the last one.
+// 3) Creates a new collection for each sequence by iterating over the instrument set / sample
+// collection pairs. If that list is exhausted before the sequence list, it reuses the last
+// instrument set / sample collection pair.
+
+
 
 class FilegroupMatcher : public Matcher {
 public:
