4ae36a6cc9a1009ada3a52876dde614d8fd3e2f8
Mike <mikelow@users.noreply.github.com>
2025-06-24 12:23:08 -0700

    KonamiArcadeSeq: * add subroutine events F6 and F7 * add alternative percussion event DE * add unknown events DF, EB * add jump event descriptions

--- src/main/formats/KonamiArcade/KonamiArcadeSeq.h
+++ src/main/formats/KonamiArcade/KonamiArcadeSeq.h
@@ -38,9 +38,12 @@ public:
 
 private:
   u8 calculateMidiPanForK054539(u8 pan);
+  void enablePercussion(bool& flag);
+  void disablePercussion(bool& flag);
 
   bool m_inJump;
-  bool m_percussion;
+  bool m_percussionFlag1;
+  bool m_percussionFlag2;
   u8 m_releaseRate;
   u8 m_curProg;
   u32 m_loopMarker[2] = {};
@@ -51,5 +54,9 @@ private:
   u8 m_duration;
   u8 m_vol;
   u8 m_pan;
+  u32 m_subroutineOffset;
+  u32 m_subroutineReturnOffset;
+  bool m_needsSubroutineEnd = false;
+  bool m_inSubRoutine = false;
   u32 m_jumpReturnOffset;
 };
