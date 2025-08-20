7104ac3b738ee9f341579fe4155f8c1da4c3f597
Mike <mikelow@users.noreply.github.com>
2025-08-20 00:13:25 -0400

    update collection without sample collection warning text

--- src/ui/qt/ManualCollectionDialog.cpp
+++ src/ui/qt/ManualCollectionDialog.cpp
@@ -179,7 +179,7 @@ void ManualCollectionDialog::createCollection() {
     }
   }
   if (coll->sampColls().empty() && coll->instrSets().front()->sampColl == nullptr) {
-    pRoot->UI_toast("No sample collections exist in the user-defined collection. "
+    pRoot->UI_toast("The created collection does not contain a sample collection. "
                     "The instrument bank will be silent.", ToastType::Warning);
   }
 
