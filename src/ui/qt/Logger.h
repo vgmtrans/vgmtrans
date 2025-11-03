738dd3fc5eeef22d0bb541b45096a77ad691f792
mikelow <mikelow@users.noreply.github.com>
2025-11-03 14:43:55 -0500

    Logger improvements: - use QTextCursor and insertText over appendHtml for better performance - flush logs after a timer interval to mitigate draw performance issues - prefix logs with level type. Only color the prefix

--- src/ui/qt/Logger.h
+++ src/ui/qt/Logger.h
@@ -6,11 +6,15 @@
 #pragma once
 
 #include <QDockWidget>
+#include <QVector>
+#include <QString>
 
 class QPlainTextEdit;
 class QComboBox;
 class QPushButton;
+class QTimer;
 class LogItem;
+enum LogLevel : int;
 
 class Logger : public QDockWidget {
   Q_OBJECT
@@ -18,15 +22,22 @@ class Logger : public QDockWidget {
 public:
   explicit Logger(QWidget *parent = nullptr);
 
-  void push(const LogItem *item) const;
+  void push(const LogItem *item);
 
-signals:
-  void closeEvent(QCloseEvent *) override;
+  signals:
+    void closeEvent(QCloseEvent *) override;
 
 private:
   void createElements();
   void connectElements();
   void exportLog();
+  void clearLog();
+  void flushPending();
+
+  struct PendingMessage {
+    QString text;
+    LogLevel level;
+  };
 
   QWidget *logger_wrapper;
   QPlainTextEdit *logger_textarea;
@@ -36,4 +47,6 @@ private:
   QPushButton *logger_save;
 
   int m_level;
+  QTimer *m_flushTimer;
+  QVector<PendingMessage> m_pendingMessages;
 };
