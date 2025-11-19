82af5da098176cca22d42b72a421f4d5b5bf461a
Mike <mikelow@users.noreply.github.com>
2025-11-19 11:43:40 +0000

    beginning of tmnt2 support

---
+++ src/main/formats/KonamiTMNT2/KonamiTMNT2Format.h
@@ -0,0 +1,13 @@
+/*
+ * VGMTrans (c) 2002-2025
+ * Licensed under the zlib license,
+ * refer to the included LICENSE.txt file
+ */
+#pragma once
+
+#include "Format.h"
+#include "KonamiTMNT2Scanner.h"
+
+BEGIN_FORMAT(KonamiTMNT2)
+  USING_SCANNER(KonamiTMNT2Scanner)
+END_FORMAT()
