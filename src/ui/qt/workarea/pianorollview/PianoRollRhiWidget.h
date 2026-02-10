6f61fefc5727f9c0407e6aff000d168c9116bd26
Mike <mikelow@users.noreply.github.com>
2026-02-09 19:42:47 -0500

    Add RHI piano roll view for sequence playback

---
+++ src/ui/qt/workarea/pianorollview/PianoRollRhiWidget.h
@@ -0,0 +1,37 @@
+/*
+ * VGMTrans (c) 2002-2026
+ * Licensed under the zlib license,
+ * refer to the included LICENSE.txt file
+ */
+
+#pragma once
+
+#include <QRhiWidget>
+
+#include "PianoRollRhiRenderer.h"
+
+class QMouseEvent;
+class QResizeEvent;
+class QWheelEvent;
+class PianoRollView;
+
+class PianoRollRhiWidget final : public QRhiWidget {
+public:
+  explicit PianoRollRhiWidget(PianoRollView* view, QWidget* parent = nullptr);
+  ~PianoRollRhiWidget() override;
+
+protected:
+  void initialize(QRhiCommandBuffer* cb) override;
+  void render(QRhiCommandBuffer* cb) override;
+  void releaseResources() override;
+  void resizeEvent(QResizeEvent* event) override;
+
+  void wheelEvent(QWheelEvent* event) override;
+  void mousePressEvent(QMouseEvent* event) override;
+  void mouseMoveEvent(QMouseEvent* event) override;
+  void mouseReleaseEvent(QMouseEvent* event) override;
+
+private:
+  PianoRollView* m_view = nullptr;
+  PianoRollRhiRenderer m_renderer;
+};
