18c0a3ce43b9d2eb93a87a717cfdb2bbcb7d4e2e
Mike <mikelow@users.noreply.github.com>
2026-02-13 15:34:21 -0500

    Refactor and simplify note preview pipeline

--- src/ui/qt/workarea/hexview/HexView.h
+++ src/ui/qt/workarea/hexview/HexView.h
@@ -66,8 +66,8 @@ public:
 signals:
   void selectionChanged(VGMItem* item);
   void seekToEventRequested(VGMItem* item);
-  void modifierNotePreviewRequested(VGMItem* item, bool includeActiveNotesAtTick);
-  void modifierNotePreviewStopped();
+  void notePreviewRequested(VGMItem* item, bool includeActiveNotesAtTick);
+  void notePreviewStopped();
 
 protected:
   bool viewportEvent(QEvent* event) override;
@@ -148,7 +148,7 @@ private:
   void updateHighlightState(bool animateSelection);
   void showTooltip(VGMItem* item, const QPoint& pos);
   void hideTooltip();
-  void stopModifierNotePreview();
+  void stopNotePreview();
 
   VGMFile* m_vgmfile = nullptr;
   // Interaction state.
