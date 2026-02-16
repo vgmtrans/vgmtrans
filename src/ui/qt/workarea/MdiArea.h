86d1dbea7cbed39576062e0c0ae0584a770712a8
Mike <mikelow@users.noreply.github.com>
2026-02-16 18:07:39 -0500

    fix mdiarea buttons

--- src/ui/qt/workarea/MdiArea.h
+++ src/ui/qt/workarea/MdiArea.h
@@ -74,6 +74,7 @@ private:
   PaneActions m_leftPaneActions{};
   PaneActions m_rightPaneActions{};
   int m_reservedTabBarRightMargin = 0;
+  QPixmap m_cachedStableTabBarColumn;
 
   std::unordered_map<const VGMFile *, QMdiSubWindow *> fileToWindowMap;
   std::unordered_map<QMdiSubWindow *, VGMFile *> windowToFileMap;
