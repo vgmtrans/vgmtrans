9477622e6045298718ea420284681c83f3fa9ba9
Mike <mikelow@users.noreply.github.com>
2026-02-17 17:06:42 -0500

    Sync selected file when VGMFileView gains focus

--- src/ui/qt/workarea/VGMFileView.cpp
+++ src/ui/qt/workarea/VGMFileView.cpp
@@ -34,6 +34,7 @@
 #include "SeqEvent.h"
 #include "SeqNoteUtils.h"
 #include "SeqTrack.h"
+#include "services/NotificationCenter.h"
 
 namespace {
 constexpr uint8_t kActiveNotePreviewVelocity = 127;
@@ -255,6 +256,7 @@ VGMFileView::PanelUi VGMFileView::createPanel(PanelSide side, bool isSeqFile) {
 
 void VGMFileView::focusInEvent(QFocusEvent* event) {
   QMdiSubWindow::focusInEvent(event);
+  NotificationCenter::the()->selectVGMFile(m_vgmfile, MdiArea::the());
 
   for (PanelSide side : {PanelSide::Left, PanelSide::Right}) {
     const auto& panelUi = panel(side);
