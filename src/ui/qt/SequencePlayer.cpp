b0e0a56469d58eda4e72f1bbb8de214ee7163300
Mike <mikelow@users.noreply.github.com>
2026-03-16 21:19:30 -0400

    Fix SequencePlayer SF2 blob ownership

--- src/ui/qt/SequencePlayer.cpp
+++ src/ui/qt/SequencePlayer.cpp
@@ -195,16 +195,17 @@ bool SequencePlayer::loadCollection(const VGMColl *coll, bool startPlaying) {
   auto rawSF2 = sf2->saveToMem();
   delete sf2;
   /* Deleted by MemFile::mem_close */
-  auto sf2_data_blob = new MemFile::DataBlob{0, std::move(rawSF2)};
+  auto sf2_data_blob = std::make_unique<MemFile::DataBlob>(MemFile::DataBlob{0, std::move(rawSF2)});
 
   /* Init soundfont */
   HSOUNDFONT sf2_handle =
-      BASS_MIDI_FontInitUser(&memory_file_callbacks, sf2_data_blob, BASS_MIDI_FONT_XGDRUMS);
+      BASS_MIDI_FontInitUser(&memory_file_callbacks, sf2_data_blob.get(), BASS_MIDI_FONT_XGDRUMS);
   if (BASS_ErrorGetCode() != BASS_OK) {
     L_ERROR("Could not load soundfont. Maybe the system is running out of "
                                   "memory or the sountfont was too large?");
     return false;
   }
+  sf2_data_blob.release();
 
   auto midi = std::unique_ptr<MidiFile>(seq->convertToMidi(coll));
   if (!midi) {
