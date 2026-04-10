833004a669678e66b757efe1e9d2faa8cf551a41
Mike <mikelow@users.noreply.github.com>
2026-04-10 14:28:58 -0400

    add drumkit header items for SD3

--- src/main/formats/SuzukiSnes/SuzukiSnesSeq.cpp
+++ src/main/formats/SuzukiSnes/SuzukiSnesSeq.cpp
@@ -43,10 +43,7 @@ bool SuzukiSnesSeq::parseHeader() {
   setPPQN(SEQ_PPQN);
 
   VGMHeader *header = addHeader(offset(), 0);
-  uint32_t curOffset = offset();
-
-  // percussion table is actually parsed by SuzukiSnesDrumKit instead
-  if (version != SUZUKISNES_SD3) {
+  auto addDrumKitChildren = [this, header](uint32_t &curOffset) -> bool {
     while (true) {
       if (curOffset + 1 >= 0x10000) {
         return false;
@@ -54,35 +51,53 @@ bool SuzukiSnesSeq::parseHeader() {
 
       uint8_t firstByte = readByte(curOffset);
       if (firstByte >= 0x80) {
-        header->addChild(curOffset, 1, "Percussion End");
+        header->addChild(curOffset, 1, "Drum Kit End");
         curOffset++;
         break;
       }
-      auto drum = header->addChild(curOffset, 5, "Percussion");
+      auto drum = header->addChild(curOffset, 5, "Drum Instrument");
       drum->addChild(curOffset++, 1, "Note Index");
       drum->addChild(curOffset++, 1, "Base Instrument");
       drum->addChild(curOffset++, 1, "Unity Key");
       drum->addChild(curOffset++, 1, "Volume");
       drum->addChild(curOffset++, 1, "Pan");
     }
+
+    return true;
+  };
+
+  auto addTrackPointerChildren = [this, header](uint32_t curOffset) {
+    for (int trackIndex = 0; trackIndex < MAX_TRACKS; trackIndex++) {
+      uint16_t addrTrackStart = readShort(curOffset);
+
+      if (addrTrackStart != 0) {
+        auto trackName = fmt::format("Track Pointer {:d}", trackIndex + 1);
+        header->addChild(curOffset, 2, trackName);
+
+        aTracks.push_back(new SuzukiSnesTrack(this, addrTrackStart));
+      }
+      else {
+        // example: Super Mario RPG - Where Am I Going?
+        header->addChild(curOffset, 2, "NULL");
+      }
+
+      curOffset += 2;
+    }
+  };
+
+  if (version == SUZUKISNES_SD3) {
+    addTrackPointerChildren(offset());
+    uint32_t curOffset = offset() + MAX_TRACKS * 2;
+    if (!addDrumKitChildren(curOffset)) {
+      return false;
+    }
   }
-
-  // create tracks
-  for (int trackIndex = 0; trackIndex < MAX_TRACKS; trackIndex++) {
-    uint16_t addrTrackStart = readShort(curOffset);
-
-    if (addrTrackStart != 0) {
-      auto trackName = fmt::format("Track Pointer {:d}", trackIndex + 1);
-      header->addChild(curOffset, 2, trackName);
-
-      aTracks.push_back(new SuzukiSnesTrack(this, addrTrackStart));
+  else {
+    uint32_t curOffset = offset();
+    if (!addDrumKitChildren(curOffset)) {
+      return false;
     }
-    else {
-      // example: Super Mario RPG - Where Am I Going?
-      header->addChild(curOffset, 2, "NULL");
-    }
-
-    curOffset += 2;
+    addTrackPointerChildren(curOffset);
   }
 
   header->setGuessedLength();
